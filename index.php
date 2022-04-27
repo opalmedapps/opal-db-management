ec28134be8d808a95ce6de208955373970158a5e
Stacey Beard <stacey.beard@muhc.mcgill.ca>
2022-04-27 18:56:06 +0000

    Add a landing page to navigate to the dbv dashboards
    This landing page is accessible via Docker ("open in browser" button), to facilitate visiting the dbv dashboards without having to copy-paste links from the README.
    
    See merge request opalmedapps/db-docker!6

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- Landing page shown when opening the dbv-apps container in a browser -->
+<html>
+<body>
+
+<h1>DBV Dashboards</h1>
+
+<p><a href="http://localhost:<?php echo getenv("PHP_PORT") ?>/dbv/dbv_opaldb/">OpalDB</a></p>
+<p><a href="http://localhost:<?php echo getenv("PHP_PORT") ?>/dbv/dbv_questionnairedb/">QuestionnaireDB</a></p>
+<p><a href="http://localhost:<?php echo getenv("PHP_PORT") ?>/dbv/dbv_registerdb/">registerdb</a></p>
+
+</body>
+</html>
