993a73bd9e919583e9394f4f8f4fd5bc3490ecf5
Napparific <kelly.agnew@mail.mcgill.ca>
2022-12-16 10:54:25 -0500

    rename folders

---
+++ alembic-opaldb/migrations/versions/a7b8dd1c55b1_generate_initial_opaldb_structure_ddl_.py
@@ -0,0 +1,122 @@
+"""Generate initial OpalDB structure DDL & DML
+
+This migration assumes the database has been created and user as write priveleges.
+
+Revision ID: a7b8dd1c55b1
+Revises: 
+Create Date: 2022-12-16 10:36:32.095480
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+from pathlib import Path
+import os
+import mariadb
+import mariadb.constants.CLIENT as CLIENT
+from dotenv import load_dotenv
+import sys
+
+# revision identifiers, used by Alembic.
+revision = 'a7b8dd1c55b1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+# DEVELOPER NOTE 
+# For this initial migration only, we will manually connect to the database and generate the schema.
+# As of 2022-12-16, we have copied the DDL & DML from dbv_opaldb, for future revisions please use alembic & SQLAlchemy ORM paradigm. 
+# See example files & README for guides on how to use these tools.
+
+# Find root and revision data paths
+ROOT_DIR = Path(__file__).parents[1]
+REVISIONS_DIR = APPS_DIR = ROOT_DIR / 'revision_data'
+# Load db connection environment variables
+load_dotenv()
+HOST = os.getenv('DOCKER_HOST')
+PORT = int(os.getenv('MARIADB_PORT'))
+USER = os.getenv('MARIADB_USER')
+PASS = os.getenv('MARIADB_PASSWORD')
+OPALDB = os.getenv('LEGACY_OPAL_DB_NAME')
+QSTDB = os.getenv('LEGACY_QUESTIONNAIRE_DB_NAME')
+
+def get_connection_cursor(autocommit: bool):
+    """Get a mariadb connection context manager for SQL execution."""
+    try:
+        conn = mariadb.connect(
+            user=USER, 
+            password=PASS, 
+            host=HOST, 
+            port=PORT, 
+            database=OPALDB, 
+            client_flag=CLIENT.MULTI_STATEMENTS,
+            autocommit=autocommit
+        )
+        return conn.cursor()
+    except mariadb.Error as e:
+        print(f'Error getting cursor or connection to mariaDB (Database {DB}) {e}')
+        sys.exit(1)
+
+
+def upgrade() -> None:
+    """Insert all data & SQL scripts for dev OpalDB."""
+    with get_connection_cursor(autocommit=True) as cursor:
+        cursor.execute(
+        """
+        SET foreign_key_checks=0;
+        SET sql_mode='';
+        SET global sql_mode='';
+        """
+        )
+
+    for idx in range(0, 12):
+        rev_folder = os.path.join(REVISIONS_DIR, str(idx))
+        for rev_file in os.listdir(rev_folder):
+            if rev_file.endswith('.sql'):
+                print(rev_file)
+    # with Path(data_path,encoding='utf8').open(encoding='utf8') as handle:
+    #     sql_content = handle.read()
+    #     with get_connection_cursor(autocommit=True) as cursor:
+    #         cursor.execute(sql_content)
+    #     handle.close()
+
+
+
+
+    with get_connection_cursor(autocommit=True) as cursor:
+       cursor.execute(
+        """
+        SET foreign_key_checks = 1;
+        SET SQL_MODE='ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+        SET GLOBAL SQL_MODE = 'ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+        """
+        )
+
+def downgrade() -> None:
+    with get_connection_cursor(autocommit=True) as cursor:
+        cursor.execute(
+        """
+        SET foreign_key_checks=0;
+        SET sql_mode='';
+        SET global sql_mode='';
+        """
+        )
+
+    with get_connection_cursor(autocommit=True) as cursor:
+        cursor.execute(
+        f"""
+        DROP {OPALDB};
+        DROP {QSTDB};
+
+        
+        """
+        )
+
+    with get_connection_cursor(autocommit=True) as cursor:
+        cursor.execute(
+        """
+        SET foreign_key_checks = 1;
+        SET SQL_MODE='ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+        SET GLOBAL SQL_MODE = 'ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+        """
+        )
