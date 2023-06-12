516ab4717a8895d18523263dfd2a1b2fe0a13553
Kelly Agnew <kelly.agnew@mail.mcgill.ca>
2023-06-12 19:17:45 +0000

    refactor(data): Data changes for demo
    See merge request opalmedapps/db-docker!103

--- db_management/opaldb/data/test/diagnosis.sql
+++ db_management/opaldb/data/test/diagnosis.sql
@@ -1,6 +1,4 @@
 INSERT INTO `Diagnosis` (`DiagnosisSerNum`, `PatientSerNum`, `SourceDatabaseSerNum`, `DiagnosisAriaSer`, `DiagnosisCode`, `Description_EN`, `Description_FR`, `Stage`, `StageCriteria`, `CreationDate`, `createdBy`, `LastUpdated`, `updatedBy`) VALUES
-(1,	51,	1,	'13956',	'C50.1',	'Ca of central portion of breast',	'',	NULL,	NULL,	'2011-05-01 00:00:00',	'CronJob',	'2023-01-12 16:39:16',	'CronJob'),
-(4,	51,	1,	'23314',	'C07',	'Ca of parotid gland',	'',	NULL,	NULL,	'2010-12-20 00:00:00',	'CronJob',	'2023-01-12 16:39:16',	'CronJob'),
-(5,	51,	1,	'22324',	'C16.9',	'Stomach, NOS',	'',	NULL,	NULL,	'2015-02-20 00:00:00',	'CronJob',	'2023-01-12 16:39:16',	'CronJob'),
-(6,	52,	1,	'23314',	'C07',	'Ca of parotid gland',	'',	NULL,	NULL,	'2010-12-20 00:00:00',	'CronJob',	'2023-01-12 16:39:16',	'CronJob'),
-(7,	53,	1,	'22324',	'C16.9',	'Stomach, NOS',	'',	NULL,	NULL,	'2015-02-20 00:00:00',	'CronJob',	'2023-01-12 16:39:16',	'CronJob');
+(1,	51,	1,	'13956',	'C50.1',	'Ca of central portion of breast',	'',	NULL,	NULL,	'2023-02-01 00:00:00',	'CronJob',	'2023-02-02 16:39:16',	'CronJob'),
+(2,	52,	1,	'23314',	'C07',	'Ca of parotid gland',	'',	NULL,	NULL,	'2023-03-20 00:00:00',	'CronJob',	'2023-03-21 16:39:16',	'CronJob'),
+(3,	53,	1,	'22324',	'C16.9',	'Stomach, NOS',	'',	NULL,	NULL,	'2023-05-12 00:00:00',	'CronJob',	'2023-05-12 16:39:16',	'CronJob');
