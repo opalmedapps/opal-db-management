92df56dea9099946ea5d90ed20f9a164ef18284e
Kelly Agnew <kelly.agnew@mail.mcgill.ca>
2023-06-19 13:16:40 +0000

    refactor: alter Doctor & DoctorMH phone fields
    See merge request opalmedapps/db-docker#108

---
+++ db_management/opaldb/versions/2023_06_15-759547f5ca7d_alter_doctor_phone_varchar.py
@@ -0,0 +1,52 @@
+"""alter_doctor_phone_varchar
+
+Revision ID: 759547f5ca7d
+Revises: 295142b4e158
+Create Date: 2023-06-15 19:38:36.276152
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision = '759547f5ca7d'
+down_revision = '295142b4e158'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Alter doctor and doctor mh to use varchar phone fields."""
+    op.alter_column(
+        'Doctor',
+        'Phone',
+        existing_type=mysql.BIGINT(display_width=20),
+        type_=sa.String(length=20),
+        existing_nullable=True,
+    )
+    op.alter_column(
+        'DoctorMH',
+        'Phone',
+        existing_type=mysql.INTEGER(display_width=20),
+        type_=sa.String(length=20),
+        existing_nullable=True,
+    )
+
+
+def downgrade() -> None:
+    """Revert varchar phone fields"""
+    op.alter_column(
+        'DoctorMH',
+        'Phone',
+        existing_type=sa.String(length=20),
+        type_=mysql.INTEGER(display_width=20),
+        existing_nullable=True,
+    )
+    op.alter_column(
+        'Doctor',
+        'Phone',
+        existing_type=sa.String(length=20),
+        type_=mysql.BIGINT(display_width=20),
+        existing_nullable=True,
+    )
