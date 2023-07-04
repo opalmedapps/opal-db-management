b12bb57759d66da3f44005538458f2ddf0194acb
Matthias Schoettle <git@mattsch.com>
2023-07-04 15:49:17 -0400

    fix(data): fix invalid queries

--- db_management/opaldb/data/test/chusj/chusj.sql
+++ db_management/opaldb/data/test/chusj/chusj.sql
@@ -9,14 +9,14 @@ UPDATE `Patient` SET `Age` = DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), `DateOfBirth`
 
 INSERT INTO `PatientControl` (`PatientSerNum`, `PatientUpdate`, `LastTransferred`, `LastUpdated`, `TransferFlag`) VALUES
 (53,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0),
-(54,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0),
+(54,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0);
 
 INSERT INTO `Patient_Hospital_Identifier` (`Patient_Hospital_Identifier_Id`, `PatientSerNum`, `Hospital_Identifier_Type_Code`, `MRN`, `Is_Active`) VALUES
 (4,	53,	'CHUSJ',	'9999996',	1),
-(5,	54,	'CHUSJ',	'9999993',	1),
+(5,	54,	'CHUSJ',	'9999993',	1);
 
 -- We add one set of Security Answers to test Django's migration scripts, they aren't actually accessed in OpalDB anymore.
 INSERT INTO `SecurityAnswer` (`SecurityQuestionSerNum`, `PatientSerNum`, `AnswerText`, `CreationDate`, `LastUpdated`) VALUES
 (1,	53,	'5ed4c7167f059c5b864fd775f527c5a88794f9f823fea73c6284756b31a08faf6f9f950473c5aa7cdb99c56bc7807517fe4c4a0bd67318bcaec508592dd6d917',	'2018-11-08 12:37:55',	'2020-12-07 00:46:00'),
 (2,	53,	'f3b49c229cc474b3334dd4a3bbe827a866cbf6d6775cde7a5c42da24b4f15db8c0e564c4ff20754841c2baa9dafffc2caa02341010456157b1de9b927f24a1e6',	'2018-11-08 12:38:23',	'2019-01-11 02:45:19'),
-(5,	53,	'a7dbabba9a0371fbdb92724a5ca66401e02069089b1f3a100374e61f934fe9e959215ae0327de2bc064a9dfc351c4d64ef89bd47e95be0198a1f466c3518cc1d',	'2018-11-08 12:38:47',	'2019-01-11 02:45:32'),
+(5,	53,	'a7dbabba9a0371fbdb92724a5ca66401e02069089b1f3a100374e61f934fe9e959215ae0327de2bc064a9dfc351c4d64ef89bd47e95be0198a1f466c3518cc1d',	'2018-11-08 12:38:47',	'2019-01-11 02:45:32');
