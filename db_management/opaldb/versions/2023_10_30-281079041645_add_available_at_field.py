7158d35cba894fcdf04b074af83d8e0878283c99
Shifeng Chen <shifeng.chen@muhc.mcgill.ca>
2023-10-30 20:33:58 -0400

    fix： add a new column to indicate the date and time to be viewed

---
+++ db_management/opaldb/versions/2023_10_30-281079041645_add_available_at_field.py
@@ -0,0 +1,32 @@
+"""add_available_at_field
+
+Revision ID: 281079041645
+Revises: ee749ce6f599
+Create Date: 2023-10-30 18:18:47.562219
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '281079041645'
+down_revision = 'd52eab1ee338'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Add new columns in PatientTestResult to indicate the date and time when it is available to be viewed."""
+    op.add_column(
+        'PatientTestResult',
+        sa.Column(
+            'AvailableAt',
+            sa.DateTime(),
+            nullable=False,
+        ),
+    )
+
+
+def downgrade() -> None:
+    """Revert the above changes."""
+    op.drop_column('PatientTestResult', 'AvailableAt')
