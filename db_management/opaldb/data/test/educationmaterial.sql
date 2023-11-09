5e497dcc5d2489847353d5c4df4194abd76e129f
Kelly Agnew <kelly.agnew@mail.mcgill.ca>
2023-11-09 18:27:59 +0000

    feat: Create test data for flintstones
    See merge request opalmedapps/db-docker!200

--- db_management/opaldb/data/test/educationmaterial.sql
+++ db_management/opaldb/data/test/educationmaterial.sql
@@ -1,16 +1,21 @@
 INSERT INTO `EducationalMaterial` (`EducationalMaterialSerNum`, `CronLogSerNum`, `EducationalMaterialControlSerNum`, `PatientSerNum`, `DateAdded`, `ReadStatus`, `ReadBy`, `LastUpdated`) VALUES
 -- all get: treatment guidelines,
-(1,	NULL,	105,	51,	'2023-02-05 08:00:55',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(2,	NULL,	105,	52,	'2023-03-14 08:00:55',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(3,	NULL,	105,	53,	'2023-05-15 08:00:55',	0,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
+(1,	NULL,	105,	51,	'2023-02-05 08:00:55',	0,	'[]',	'2023-01-12 16:39:17'),
+(2,	NULL,	105,	52,	'2023-03-14 08:00:55',	0,	'[]',	'2023-01-12 16:39:17'),
+(3,	NULL,	105,	53,	'2023-05-15 08:00:55',	0,	'[]',	'2023-01-12 16:39:17'),
 
 -- marge: intro to breast cancer, covid care guide,fertility preservation for women
-(4,	NULL,	960,	51,	'2023-02-15 16:31:26',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(5,	NULL,	849,	51,	'2023-02-21 14:57:18',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(6,	NULL,	643,	51,	'2023-03-30 10:20:11',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
+(4,	NULL,	960,	51,	'2023-02-15 16:31:26',	0,	'[]',	'2023-01-12 16:39:17'),
+(5,	NULL,	849,	51,	'2023-02-21 14:57:18',	0,	'[]',	'2023-01-12 16:39:17'),
+(6,	NULL,	643,	51,	'2023-03-30 10:20:11',	0,	'[]',	'2023-01-12 16:39:17'),
 
 -- homer: covid care guide, fertility preservation for men
-(7,	NULL,	849,	52,	'2023-03-14 14:57:18',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(8,	NULL,	642,	52,	'2023-04-05 10:11:54',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17');
+(7,	NULL,	849,	52,	'2023-03-14 14:57:18',	0,	'[]',	'2023-01-12 16:39:17'),
+(8,	NULL,	642,	52,	'2023-04-05 10:11:54',	0,	'[]',	'2023-01-12 16:39:17'),
 
--- bart: no extras
+-- fred: fertility preservation for men
+(9,	NULL,	642,	56,	'2023-10-05 10:11:54',	0,	'[]',	'2023-01-12 16:39:17'),
+
+-- pebbles: treatment guidelines
+(10,	NULL,	105,	57,	'2023-10-15 08:00:55',	0,	'[]',	'2023-01-12 16:39:17');
+
