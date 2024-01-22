b099192abfb6565250ca8d780fcd587da7064ba7
Shifeng Chen <shifeng.chen@muhc.mcgill.ca>
2024-01-22 22:00:28 +0000

    fix: add AvailableAt column to indicate when a patient test result is available to be viewed
    See merge request opalmedapps/db-docker#194

---
+++ db_management/opaldb/versions/2024_01_18-281079041645_add_available_at_field.py
@@ -0,0 +1,49 @@
+"""add_available_at_field
+
+Revision ID: 281079041645
+Revises: ee749ce6f599
+Create Date: 2024-01-18
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '281079041645'
+down_revision = '224fa0bbfd7c'
+branch_labels = None
+depends_on = None
+
+
+# Ensure that the AvailableAt column have been populated with the value from column ResultDateTime.
+available_at_migration_query = """
+    UPDATE PatientTestResult ptr
+    SET ptr.AvailableAt = ptr.ResultDateTime
+    WHERE ptr.ResultDateTime IS NOT NULL
+    ;
+"""
+
+
+def upgrade() -> None:
+    """Add new columns in PatientTestResult to indicate the date and time when it is available to be viewed."""
+    op.add_column(
+        'PatientTestResult',
+        sa.Column(
+            'AvailableAt',
+            sa.DateTime(),
+            nullable=True,
+        ),
+    )
+    # Ensure the AvailableAt fields have been populated with the values of the ResultDateTime field
+    op.execute(available_at_migration_query)
+    op.alter_column(
+        'PatientTestResult',
+        'AvailableAt',
+        existing_type=sa.DateTime(),
+        nullable=False,
+    )
+
+
+def downgrade() -> None:
+    """Revert the above changes."""
+    op.drop_column('PatientTestResult', 'AvailableAt')
