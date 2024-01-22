84bdd484bb3f802a5f397d96f912d88c851768a0
Matthias Schoettle <git@mattsch.com>
2024-01-22 16:11:44 -0500

    fixes

--- tests/test_alembic.py
+++ tests/test_alembic.py
@@ -4,6 +4,14 @@ opaldb = create_alembic_fixture({'script_location': 'db_management/opaldb'})
 questionnairedb = create_alembic_fixture({'script_location': 'db_management/questionnairedb'})
 
 
+def test_questionnairedb_single_head_revision(questionnairedb):
+    tests.test_single_head_revision(questionnairedb)
+
+
+def test_questionnairedb_upgrade(questionnairedb):
+    tests.test_upgrade(questionnairedb)
+
+
 def test_opaldb_single_head_revision(opaldb):
     tests.test_single_head_revision(opaldb)
 
@@ -20,11 +28,6 @@ def test_opaldb_model_definitions_match_ddl(opaldb):
 # def test_opaldb_up_down_consistency(opaldb):
     # tests.test_up_down_consistency(opaldb)
 
-# def test_questionnairedb_single_head_revision(questionnairedb):
-#     tests.test_single_head_revision(questionnairedb)
-
-# def test_questionnairedb_upgrade(questionnairedb):
-#     tests.test_upgrade(questionnairedb)
 
 # def test_questionnairedb_model_definitions_match_ddl(questionnairedb):
 #     tests.test_model_definitions_match_ddl(questionnairedb)
