42f546ba5391926a0ac792c89c44c122b3b4ac5e
Matthias Schoettle <git@mattsch.com>
2024-01-22 21:27:06 -0500

    add downgrade

--- db_management/opaldb/versions/2023_05_19-7ebe01c0c5e0_add_default_value_for_alert_deletedby.py
+++ db_management/opaldb/versions/2023_05_19-7ebe01c0c5e0_add_default_value_for_alert_deletedby.py
@@ -1,10 +1,11 @@
-"""Add default value for alert deletedBy
+"""Add default value for alert deletedBy.
 
 Revision ID: 7ebe01c0c5e0
 Revises: f231a7f6f6ca
 Create Date: 2023-05-19 16:57:01.636308
 
 """
+import sqlalchemy as sa
 from alembic import op
 from sqlalchemy.dialects import mysql
 
@@ -21,6 +22,19 @@ def upgrade() -> None:
         'alert',
         'deletedBy',
         existing_type=mysql.VARCHAR(length=128),
-        nullable=True,
+        server_default=sa.text("''"),
+        existing_nullable=False,
+        existing_comment='Username of the person who deleted the record',
+    )
+
+
+def downgrade() -> None:
+    """Alter deletedBy in Alert to have proper default."""
+    op.alter_column(
+        'alert',
+        'deletedBy',
+        existing_type=mysql.VARCHAR(length=128),
+        server_default=None,
+        existing_nullable=False,
         existing_comment='Username of the person who deleted the record',
     )
