5007cc8631c685976e0d09612d4a0a924d9f0ee8
Matthias Schoettle <git@mattsch.com>
2024-01-22 21:27:37 -0500

    fixes

--- tests/test_alembic.py
+++ tests/test_alembic.py
@@ -17,6 +17,10 @@ def test_questionnairedb_model_definitions_match_ddl(questionnairedb):
     tests.test_model_definitions_match_ddl(questionnairedb)
 
 
+def test_questionnairedb_up_down_consistency(questionnairedb):
+    tests.test_up_down_consistency(questionnairedb)
+
+
 def test_opaldb_single_head_revision(opaldb):
     tests.test_single_head_revision(opaldb)
 
@@ -26,16 +30,7 @@ def test_opaldb_upgrade(opaldb):
 
 
 def test_opaldb_model_definitions_match_ddl(opaldb):
-    # print(opaldb.config)
-    # assert False
     tests.test_model_definitions_match_ddl(opaldb)
 
-# def test_opaldb_up_down_consistency(opaldb):
-    # tests.test_up_down_consistency(opaldb)
-
-
-# def test_questionnairedb_model_definitions_match_ddl(questionnairedb):
-#     tests.test_model_definitions_match_ddl(questionnairedb)
-
-# def test_questionnairedb_up_down_consistency(questionnairedb):
-#     tests.test_up_down_consistency(questionnairedb)
+def test_opaldb_up_down_consistency(opaldb):
+    tests.test_up_down_consistency(opaldb)
