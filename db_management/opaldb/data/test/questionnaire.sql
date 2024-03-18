7bc1abcd51106668d37fcac0c8df76c3f3d783d7
Napparific <kelly.agnew@mail.mcgill.ca>
2024-03-18 15:18:40 -0400

    update questionnaire and notification dates, complete marges preop questionnaire

--- db_management/opaldb/data/test/questionnaire.sql
+++ db_management/opaldb/data/test/questionnaire.sql
@@ -1,27 +1,114 @@
 INSERT INTO `Questionnaire` (`QuestionnaireSerNum`, `CronLogSerNum`, `QuestionnaireControlSerNum`, `PatientSerNum`, `DateAdded`, `PatientQuestionnaireDBSerNum`, `CompletedFlag`, `CompletionDate`, `SessionId`, `LastUpdated`) VALUES
 -- all get: esas
-(1,	NULL,	4,	51,	'2023-05-05 10:13:07',	184,	0,	NULL,	'',	'2021-10-05 17:27:07'),
-(2,	NULL,	4,	52,	'2023-05-05 10:13:07',	190,	0,	NULL,	'',	'2021-10-05 17:27:07'),
-(3,	NULL,	4,	53,	'2023-05-05 10:13:07',	207,	0,	NULL,	'',	'2021-10-05 17:27:07'),
+(1,	NULL,	4,	51,	'2000-01-01 00:00:00',	184,	0,	NULL,	'',	'2000-01-01 00:00:00'),
+(2,	NULL,	4,	52,	'2000-01-01 00:00:00',	190,	0,	NULL,	'',	'2000-01-01 00:00:00'),
+(3,	NULL,	4,	53,	'2000-01-01 00:00:00',	207,	0,	NULL,	'',	'2000-01-01 00:00:00'),
 
--- marge: breast
-(4,	NULL,	155,	51,	'2023-03-05 17:43:45',	5354,	0,	NULL,	'',	'2021-10-05 17:27:07'),
-(5,	NULL,	156,	51,	'2023-03-05 17:43:46',	5355,	0,	NULL,	'',	'2021-10-05 17:27:07'),
+-- marge: breast, preop completed
+(4,	NULL,	155,	51,	'2000-01-01 00:00:00',	5354,	1,	NULL,	'',	'2000-01-01 00:00:00'),
+(5,	NULL,	156,	51,	'2000-01-01 00:00:00',	5355,	0,	NULL,	'',	'2000-01-01 00:00:00'),
 
 -- homer: QOL head and neck
-(6,	NULL,	108,	52,	'2023-04-25 22:03:26',	3457,	0,	NULL,	'',	'2021-10-05 17:27:07'),
+(6,	NULL,	108,	52,	'2000-01-01 00:00:00',	3457,	0,	NULL,	'',	'2000-01-01 00:00:00'),
 
 -- bart: bowel function
-(7,	NULL,	24,	53,	'2023-05-15 13:34:00',	840,	0,	NULL,	'',	'2021-10-05 17:27:07'),
+(7,	NULL,	24,	53,	'2000-01-01 00:00:00',	840,	0,	NULL,	'',	'2000-01-01 00:00:00'),
 
 -- all get databank consent questionnaire
-(8,	85504,	157,	51,	'2023-09-22 11:02:13',	5359,	0,	NULL,	'[]',	'2023-09-22 15:05:05'),
-(9,	85504,	157,	52,	'2023-09-22 11:02:16',	5360,	0,	NULL,	'[]',	'2023-09-22 15:03:11'),
-(10,	85504,	157,	53,	'2023-09-22 11:02:19',	5361,	0,	NULL,	'[]',	'2023-09-22 15:03:11'),
-(11,	85504,	157,	55,	'2023-09-22 11:02:21',	5362,	0,	NULL,	'[]',	'2023-09-22 15:03:11'),
-(12,	85504,	157,	56,	'2023-09-22 11:02:21',	5363,	0,	NULL,	'[]',	'2023-09-22 15:03:11'),
-(13,	85504,	157,	57,	'2023-09-22 11:02:21',	5364,	0,	NULL,	'[]',	'2023-09-22 15:03:11'),
+(8,	85504,	157,	51,	'2000-01-01 00:00:00',	5359,	0,	NULL,	'',	'2000-01-01 00:00:00'),
+(9,	85504,	157,	52,	'2000-01-01 00:00:00',	5360,	0,	NULL,	'',	'2000-01-01 00:00:00'),
+(10,	85504,	157,	53,	'2000-01-01 00:00:00',	5361,	0,	NULL,	'',	'2000-01-01 00:00:00'),
+(11,	85504,	157,	55,	'2000-01-01 00:00:00',	5362,	0,	NULL,	'',	'2000-01-01 00:00:00'),
+(12,	85504,	157,	56,	'2000-01-01 00:00:00',	5363,	0,	NULL,	'',	'2000-01-01 00:00:00'),
+(13,	85504,	157,	57,	'2000-01-01 00:00:00',	5364,	0,	NULL,	'',	'2000-01-01 00:00:00'),
 
 -- fred: GAD7
-(14,	85504,	50,	56,	'2023-09-22 11:02:21',	5365,	0,	NULL,	'[]',	'2023-09-22 15:03:11');
+(14,	85504,	50,	56,	'2000-01-01 00:00:00',	5365,	0,	NULL,	'',	'2000-01-01 00:00:00');
 
+-- Update all Questionnaire dates to make the data more similar to a live environment
+
+-- ESAS sent 3 days ago
+UPDATE Questionnaire
+SET DateAdded = DATE_ADD(now(), INTERVAL -3 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -3 DAY)
+WHERE QuestionnaireControlSerNum = 4;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -3 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -3 DAY)
+WHERE NotificationControlSerNum = 13
+AND RefTableRowSerNum in (1, 2, 3);
+
+-- Breast Recon: Preop complete 7 days ago; Postop sent 3 days ago
+UPDATE Questionnaire
+SET DateAdded = DATE_ADD(now(), INTERVAL -7 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -7 DAY),
+    CompletionDate = DATE_ADD(now(), INTERVAL -6 DAY)
+WHERE QuestionnaireControlSerNum = 155;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -7 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -7 DAY)
+WHERE NotificationControlSerNum = 13
+AND RefTableRowSerNum = 4;
+
+UPDATE Questionnaire
+SET DateAdded = DATE_ADD(now(), INTERVAL -3 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -3 DAY)
+WHERE QuestionnaireControlSerNum = 156;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -3 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -3 DAY)
+WHERE NotificationControlSerNum = 13
+AND RefTableRowSerNum = 5;
+
+-- H&N Quality sent yesterday
+UPDATE Questionnaire
+SET DateAdded = DATE_ADD(now(), INTERVAL -1 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -1 DAY)
+WHERE QuestionnaireControlSerNum = 108;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -1 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -1 DAY)
+WHERE NotificationControlSerNum = 13
+AND RefTableRowSerNum = 6;
+
+-- Bowel function sent 5 days ago
+UPDATE Questionnaire
+SET DateAdded = DATE_ADD(now(), INTERVAL -5 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -5 DAY)
+WHERE QuestionnaireControlSerNum = 24;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -5 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -5 DAY)
+WHERE NotificationControlSerNum = 13
+AND RefTableRowSerNum = 7;
+
+-- Databank Consent sent 2 days ago
+UPDATE Questionnaire
+SET DateAdded = DATE_ADD(now(), INTERVAL -2 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -2 DAY)
+WHERE QuestionnaireControlSerNum = 157;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -2 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -2 DAY)
+WHERE NotificationControlSerNum = 13
+AND RefTableRowSerNum IN (8, 9, 10, 11, 12, 13);
+
+-- GAD7 sent 7 days ago
+UPDATE Questionnaire
+SET DateAdded = DATE_ADD(now(), INTERVAL -7 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -7 DAY)
+WHERE QuestionnaireControlSerNum = 50;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -7 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -7 DAY)
+WHERE NotificationControlSerNum = 13
+AND RefTableRowSerNum = 14;
+
+-- Set Marge's preop notification to `Read` following the insert operation trigger
+UPDATE Notification
+SET ReadStatus = 1,
+    ReadBy = '["QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2"]'
+WHERE
+    PatientSerNum = 51
+AND NotificationControlSerNum = 13
+AND RefTableRowSerNum = 4;
