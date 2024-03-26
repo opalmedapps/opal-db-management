72ddffa48d0e2fffd13c6f8415870abd34d844f9
Kelly Agnew <kelly.agnew@mail.mcgill.ca>
2024-03-26 17:09:03 +0000

    fix: remove old usernames from data
    See merge request opalmedapps/db-docker!293

--- db_management/opaldb/data/test/txteammessage.sql
+++ db_management/opaldb/data/test/txteammessage.sql
@@ -1,4 +1,4 @@
 INSERT INTO `TxTeamMessage` (`TxTeamMessageSerNum`, `CronLogSerNum`, `PatientSerNum`, `PostControlSerNum`, `DateAdded`, `ReadStatus`, `ReadBy`, `LastUpdated`) VALUES
-(1,	NULL,	51,	13,	'2023-02-28 00:00:00',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(2,	NULL,	52,	13,	'2023-04-11 00:00:00',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(3,	NULL,	53,	13,	'2023-05-12 00:00:00',	0,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17');
+(1,	NULL,	51,	13,	'2023-02-28 00:00:00',	0,	'[]',	'2023-01-12 16:39:17'),
+(2,	NULL,	52,	13,	'2023-04-11 00:00:00',	0,	'[]',	'2023-01-12 16:39:17'),
+(3,	NULL,	53,	13,	'2023-05-12 00:00:00',	0,	'[]',	'2023-01-12 16:39:17');
