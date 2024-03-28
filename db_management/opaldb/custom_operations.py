a1fcf74c4d2f265029976a1ebf6d5ac6c49ada61
Matthias Schoettle <git@mattsch.com>
2024-03-27 22:26:22 -0400

    create and drop procedures

--- db_management/opaldb/custom_operations.py
+++ db_management/opaldb/custom_operations.py
@@ -10,7 +10,7 @@ class ReplaceableObject:
     Design copied from Alembic cookbook: https://alembic.sqlalchemy.org/en/latest/cookbook.html#replaceable-objects
     """
 
-    def __init__(self, name: str, sqltext: str) -> None:
+    def __init__(self, name: str, sqltext: str = '') -> None:
         """Initialize with the identifier and textual definition of the entity to be created/replaced/dropped.
 
         Args:
@@ -19,6 +19,19 @@ class ReplaceableObject:
         """
         self.name = name
         self.sqltext = sqltext
+
+
+class ReversibleOp(MigrateOperation):
+    def __init__(self, target: ReplaceableObject):
+        self.target = target
+
+    @classmethod
+    def invoke_for_target(cls, operations: Operations, target: ReplaceableObject) -> Any:
+        op = cls(target)
+        return operations.invoke(op)
+
+    def reverse(self) -> MigrateOperation:
+        raise NotImplementedError()
 
 
 @Operations.register_operation('create_trigger')
@@ -71,7 +84,7 @@ class DropTriggerOp(MigrateOperation):
         self.sqltext = replaceable_obj.sqltext
 
     @classmethod
-    def drop_trigger(cls: Type['CreateTriggerOp'], operations: Operations, replaceable_obj: ReplaceableObject) -> Any:
+    def drop_trigger(cls: Type['DropTriggerOp'], operations: Operations, replaceable_obj: ReplaceableObject) -> Any:
         """Issue a "DROP TRIGGER" instruction.
 
         Args:
@@ -113,3 +126,28 @@ def drop_trigger(operations: Operations, operation: CreateTriggerOp) -> None:
         operation: DropTriggerOp instance
     """
     operations.execute('DROP TRIGGER IF EXISTS {0}'.format(operation.name))
+
+
+@Operations.register_operation('create_procedure', 'invoke_for_target')
+class CreateProcedureOp(ReversibleOp):
+    def reverse(self) -> ReversibleOp:
+        return DropProcedureOp(self.target)
+
+
+@Operations.register_operation('drop_procedure', 'invoke_for_target')
+class DropProcedureOp(ReversibleOp):
+    def reverse(self) -> ReversibleOp:
+        return CreateProcedureOp(self.target)
+
+
+@Operations.implementation_for(CreateProcedureOp)
+def create_procedure(operations: Operations, operation: ReversibleOp) -> None:
+    operations.execute("CREATE FUNCTION %s %s" % (
+        operation.target.name,
+        operation.target.sqltext,
+    ))
+
+
+@Operations.implementation_for(DropProcedureOp)
+def drop_procedure(operations: Operations, operation: ReversibleOp) -> None:
+    operations.execute("DROP FUNCTION %s" % operation.target.name)
