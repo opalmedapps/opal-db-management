6963d2b2072ee67558b04d003769393b7e2e0903
Matthias Schoettle <3097098-mschoettle@users.noreply.gitlab.com>
2024-04-02 16:12:43 +0000

    feat: remove getLevel stored procedure
    See merge request opalmedapps/db-docker!299

--- db_management/opaldb/custom_operations.py
+++ db_management/opaldb/custom_operations.py
@@ -1,7 +1,10 @@
 """Custom operations file for version controlling other SQL entities."""
-from typing import Any, Type
+from typing import Any, Final
 
 from alembic.operations import MigrateOperation, Operations
+
+INVOKE_FOR_TARGET: Final[str] = 'invoke_for_target'
+REPLACE: Final[str] = 'replace'
 
 
 class ReplaceableObject:
@@ -10,43 +13,114 @@ class ReplaceableObject:
     Design copied from Alembic cookbook: https://alembic.sqlalchemy.org/en/latest/cookbook.html#replaceable-objects
     """
 
-    def __init__(self, name: str, sqltext: str) -> None:
+    def __init__(self, name: str, sql_text: str) -> None:
         """Initialize with the identifier and textual definition of the entity to be created/replaced/dropped.
 
         Args:
             name: identifier for the entity
-            sqltext: MySQL definition of the base operation
+            sql_text: MySQL definition of the base operation
         """
         self.name = name
-        self.sqltext = sqltext
+        self.sql_text = sql_text
 
 
-@Operations.register_operation('create_trigger')
-class CreateTriggerOp(MigrateOperation):
-    """Create a Trigger."""
+class ReversibleOp(MigrateOperation):
+    """
+    Reversible operation for replaceable objects.
 
-    def __init__(self, replaceable_obj: ReplaceableObject) -> None:
-        """Extract the name and sqltext from the operation object.
+    Source: https://alembic.sqlalchemy.org/en/latest/cookbook.html#create-operations-for-the-target-objects
+    """
+
+    def __init__(self, target: ReplaceableObject):
+        """
+        Initialize the reversible operation for a replaceable object.
 
         Args:
-            replaceable_obj: a ReplaceableObject or other class wrapper with name and sqltext fields.
+            target: the replaceable object
         """
-        self.name = replaceable_obj.name
-        self.sqltext = replaceable_obj.sqltext
+        self.target = target
 
     @classmethod
-    def create_trigger(cls: Type['CreateTriggerOp'], operations: Operations, replaceable_obj: ReplaceableObject) -> Any:
-        """Issue a "CREATE TRIGGER" instruction.
+    def invoke_for_target(cls, operations: Operations, target: ReplaceableObject) -> Any:
+        """
+        Invoke the operation for the target replaceable object.
 
         Args:
-            operations: Alembic Operations instance (context in which the migration is being performed)
-            replaceable_obj: The trigger object to be created
+            operations: the operations instance
+            target: the replaceable object
 
         Returns:
-            Any return from the invocation of the operation, though not strictly used
+            the return value of invoking the operation, if any
         """
-        op = cls(replaceable_obj)
+        op = cls(target)
         return operations.invoke(op)
+
+    def reverse(self) -> MigrateOperation:
+        """
+        Reverses the operation.
+
+        Sub-classes need to implement this method.
+
+        Raises:
+            NotImplementedError: if the sub-class does not implement this method
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def replace(
+        cls,
+        operations: Operations,
+        target: ReplaceableObject,
+        replaces: str | None = None,
+        replace_with: str | None = None,
+    ) -> None:
+        """
+        Replace the existing element with an updated one.
+
+        For example, to update a stored procedure, the existing one needs to be replaced
+        by deleting the current one and creating the new one under the same name.
+
+        Either `replaces` or `replaces_with` needs to be provided.
+
+        Args:
+            operations: the operations instance
+            target: the target replaceable object
+            replaces: a reference to the replaceable object to replace in the form migration_name.attribute
+            replace_with: a reference to the replaceable object to replace with the given one (migration.attribute)
+
+        Raises:
+            TypeError: if neither the replace nor replace_with argument is provided
+        """
+        if replaces:
+            old_obj = cls._get_object_from_version(operations, replaces)
+            drop_old = cls(old_obj).reverse()
+            create_new = cls(target)
+        elif replace_with:
+            old_obj = cls._get_object_from_version(operations, replace_with)
+            drop_old = cls(target).reverse()
+            create_new = cls(old_obj)
+        else:
+            raise TypeError('replaces or replace_with is required')
+
+        operations.invoke(drop_old)
+        operations.invoke(create_new)
+
+    @classmethod
+    def _get_object_from_version(cls, operations: Operations, ident: str) -> Any:
+        version, objname = ident.split('.')
+
+        script = operations.get_context().script
+
+        if script:
+            module = script.get_revision(version).module
+            return getattr(module, objname)
+
+        raise ValueError('script directory not found')
+
+
+@Operations.register_operation('create_trigger', INVOKE_FOR_TARGET)
+class CreateTriggerOp(ReversibleOp):
+    """Create a Trigger."""
 
     def reverse(self) -> MigrateOperation:
         """Call the inverse method to reverse the effect of the CreateTrigger operation.
@@ -54,35 +128,12 @@ class CreateTriggerOp(MigrateOperation):
         Returns:
             Instance of MigrateOperation
         """
-        return DropTriggerOp(self.name, sqltext=self.sqltext)
+        return DropTriggerOp(self.target)
 
 
-@Operations.register_operation('drop_trigger')
-class DropTriggerOp(MigrateOperation):
+@Operations.register_operation('drop_trigger', INVOKE_FOR_TARGET)
+class DropTriggerOp(ReversibleOp):
     """Drop a Trigger."""
-
-    def __init__(self, replaceable_obj: ReplaceableObject) -> None:
-        """Extract the name and sqltext from the operation object.
-
-        Args:
-            replaceable_obj: a ReplaceableObject or other class wrapper with name and sqltext fields.
-        """
-        self.name = replaceable_obj.name
-        self.sqltext = replaceable_obj.sqltext
-
-    @classmethod
-    def drop_trigger(cls: Type['CreateTriggerOp'], operations: Operations, replaceable_obj: ReplaceableObject) -> Any:
-        """Issue a "DROP TRIGGER" instruction.
-
-        Args:
-            operations: Alembic Operations instance (context in which the migration is being performed)
-            replaceable_obj: The trigger object to be created
-
-        Returns:
-            Any return from the invocation of the operation, though not strictly used
-        """
-        op = cls(replaceable_obj)
-        return operations.invoke(op)
 
     def reverse(self) -> MigrateOperation:
         """Call the inverse method to reverse the effect of the DropTrigger operation.
@@ -90,7 +141,7 @@ class DropTriggerOp(MigrateOperation):
         Returns:
             Instance of MigrateOperation
         """
-        return CreateTriggerOp(self.name, sqltext=self.sqltext)
+        return CreateTriggerOp(self.target)
 
 
 @Operations.implementation_for(CreateTriggerOp)
@@ -101,7 +152,7 @@ def create_trigger(operations: Operations, operation: CreateTriggerOp) -> None:
         operations: Alembic Operations instance (context in which the migration is being performed)
         operation: CreateTriggerOp instance
     """
-    operations.execute('CREATE TRIGGER {0} {1}'.format(operation.name, operation.sqltext))
+    operations.execute('CREATE TRIGGER {0} {1}'.format(operation.target.name, operation.target.sql_text))
 
 
 @Operations.implementation_for(DropTriggerOp)
@@ -112,4 +163,60 @@ def drop_trigger(operations: Operations, operation: CreateTriggerOp) -> None:
         operations: Alembic Operations instance (context in which the migration is being performed)
         operation: DropTriggerOp instance
     """
-    operations.execute('DROP TRIGGER IF EXISTS {0}'.format(operation.name))
+    operations.execute('DROP TRIGGER IF EXISTS {0}'.format(operation.target.name))
+
+
+@Operations.register_operation('create_procedure', INVOKE_FOR_TARGET)
+@Operations.register_operation('replace_procedure', REPLACE)
+class CreateProcedureOp(ReversibleOp):
+    """Create stored procedure operation."""
+
+    def reverse(self) -> MigrateOperation:
+        """
+        Reverse the create procedure operation by returning an operation that drops the procedure.
+
+        Returns:
+            a drop procedure operation
+        """
+        return DropProcedureOp(self.target)
+
+
+@Operations.register_operation('drop_procedure', INVOKE_FOR_TARGET)
+class DropProcedureOp(ReversibleOp):
+    """Drop stored procedure operation."""
+
+    def reverse(self) -> MigrateOperation:
+        """
+        Reverse the drop procedure operation by returning an operation that creates the procedure.
+
+        Returns:
+            a create procedure operation
+        """
+        return CreateProcedureOp(self.target)
+
+
+@Operations.implementation_for(CreateProcedureOp)
+def create_procedure(operations: Operations, operation: CreateProcedureOp) -> None:
+    """
+    Execute the create stored procedure operation.
+
+    Args:
+        operations: the operations instance
+        operation: the create stored procedure operation to execute
+    """
+    operations.execute('CREATE FUNCTION {name} {definition}'.format(
+        name=operation.target.name,
+        definition=operation.target.sql_text,
+    ))
+
+
+@Operations.implementation_for(DropProcedureOp)
+def drop_procedure(operations: Operations, operation: DropProcedureOp) -> None:
+    """
+    Execute the drop stored procedure operation.
+
+    Args:
+        operations: the operations instance
+        operation: the drop stored procedure operation to execute
+    """
+    operations.execute('DROP FUNCTION {0}'.format(operation.target.name))
--- db_management/opaldb/versions/2024_02_14-627bbdebf282_remove_qr_image_file_name.py
+++ db_management/opaldb/versions/2024_02_14-627bbdebf282_remove_qr_image_file_name.py
@@ -20,7 +20,7 @@ depends_on = None
 
 UPDATED_HOSPITALMAP_DELETE_TRIGGER = ReplaceableObject(
     name='`hospitalmap_delete_trigger`',
-    sqltext="""AFTER DELETE ON `HospitalMap` FOR EACH ROW BEGIN\n
+    sql_text="""AFTER DELETE ON `HospitalMap` FOR EACH ROW BEGIN\n
 INSERT INTO `HospitalMapMH`(`HospitalMapSerNum`, `MapUrl`, `MapURL_EN`,
 `MapURL_FR`, `QRMapAlias`, `MapName_EN`, `MapDescription_EN`, `MapName_FR`,
 `MapDescription_FR`, `DateAdded`, `LastUpdatedBy`, `SessionId`, `ModificationAction`)
@@ -30,7 +30,7 @@ OLD.LastUpdatedBy, OLD.SessionId, 'DELETE');\nEND;\n""")
 
 UPDATED_HOSPITALMAP_INSERT_TRIGGER = ReplaceableObject(
     name='`hospitalmap_insert_trigger`',
-    sqltext="""AFTER INSERT ON `HospitalMap` FOR EACH ROW BEGIN\n
+    sql_text="""AFTER INSERT ON `HospitalMap` FOR EACH ROW BEGIN\n
 INSERT INTO `HospitalMapMH`(`HospitalMapSerNum`, `MapUrl`, `MapURL_EN`,
 `MapURL_FR`, `QRMapAlias`, `MapName_EN`, `MapDescription_EN`, `MapName_FR`,
 `MapDescription_FR`, `DateAdded`, `LastUpdatedBy`, `SessionId`, `ModificationAction`)
@@ -40,7 +40,7 @@ NEW.LastUpdatedBy, NEW.SessionId, 'INSERT');\nEND;\n""")
 
 UPDATED_HOSPITALMAP_UPDATE_TRIGGER = ReplaceableObject(
     name='`hospitalmap_update_trigger`',
-    sqltext="""AFTER UPDATE ON `HospitalMap` FOR EACH ROW BEGIN\n
+    sql_text="""AFTER UPDATE ON `HospitalMap` FOR EACH ROW BEGIN\n
 INSERT INTO `HospitalMapMH`(`HospitalMapSerNum`, `MapUrl`, `MapURL_EN`,
 `MapURL_FR`, `QRMapAlias`, `MapName_EN`, `MapDescription_EN`, `MapName_FR`,
 `MapDescription_FR`, `DateAdded`, `LastUpdatedBy`, `SessionId`, `ModificationAction`)
@@ -50,7 +50,7 @@ NEW.LastUpdatedBy, NEW.SessionId, 'UPDATE');\nEND;\n""")
 
 OLD_HOSPITALMAP_DELETE_TRIGGER = ReplaceableObject(
     name='`hospitalmap_delete_trigger`',
-    sqltext="""AFTER DELETE ON `HospitalMap` FOR EACH ROW BEGIN\n
+    sql_text="""AFTER DELETE ON `HospitalMap` FOR EACH ROW BEGIN\n
 INSERT INTO `HospitalMapMH`(`HospitalMapSerNum`, `MapUrl`,
 `MapURL_EN`, `MapURL_FR`, `QRMapAlias`, `QRImageFileName`, `MapName_EN`,
 `MapDescription_EN`, `MapName_FR`, `MapDescription_FR`, `DateAdded`,
@@ -62,7 +62,7 @@ OLD.SessionId, 'DELETE');\nEND;\n""")
 
 OLD_HOSPITALMAP_INSERT_TRIGGER = ReplaceableObject(
     name='`hospitalmap_insert_trigger`',
-    sqltext="""AFTER INSERT ON `HospitalMap` FOR EACH ROW BEGIN\n
+    sql_text="""AFTER INSERT ON `HospitalMap` FOR EACH ROW BEGIN\n
 INSERT INTO `HospitalMapMH`(`HospitalMapSerNum`, `MapUrl`,
 `MapURL_EN`, `MapURL_FR`, `QRMapAlias`, `QRImageFileName`, `MapName_EN`,
 `MapDescription_EN`, `MapName_FR`, `MapDescription_FR`, `DateAdded`,
@@ -74,7 +74,7 @@ NEW.SessionId, 'INSERT');\nEND;\n""")
 
 OLD_HOSPITALMAP_UPDATE_TRIGGER = ReplaceableObject(
     name='`hospitalmap_update_trigger`',
-    sqltext="""AFTER UPDATE ON `HospitalMap` FOR EACH ROW BEGIN\n
+    sql_text="""AFTER UPDATE ON `HospitalMap` FOR EACH ROW BEGIN\n
 INSERT INTO `HospitalMapMH`(`HospitalMapSerNum`, `MapUrl`,
 `MapURL_EN`, `MapURL_FR`, `QRMapAlias`, `QRImageFileName`, `MapName_EN`,
 `MapDescription_EN`, `MapName_FR`, `MapDescription_FR`, `DateAdded`,
@@ -89,21 +89,21 @@ def upgrade() -> None:
     """Update QRImageFileName column from hospitalmap and Triggers to remove QRImageFileName instance"""
     op.drop_column('HospitalMap', 'QRImageFileName')
     op.drop_column('HospitalMapMH', 'QRImageFileName')
-    op.drop_trigger(OLD_HOSPITALMAP_DELETE_TRIGGER)
-    op.drop_trigger(OLD_HOSPITALMAP_INSERT_TRIGGER)
-    op.drop_trigger(OLD_HOSPITALMAP_UPDATE_TRIGGER)
-    op.create_trigger(UPDATED_HOSPITALMAP_DELETE_TRIGGER)
-    op.create_trigger(UPDATED_HOSPITALMAP_INSERT_TRIGGER)
-    op.create_trigger(UPDATED_HOSPITALMAP_UPDATE_TRIGGER)
+    op.drop_trigger(OLD_HOSPITALMAP_DELETE_TRIGGER)  # type: ignore[attr-defined]
+    op.drop_trigger(OLD_HOSPITALMAP_INSERT_TRIGGER)  # type: ignore[attr-defined]
+    op.drop_trigger(OLD_HOSPITALMAP_UPDATE_TRIGGER)  # type: ignore[attr-defined]
+    op.create_trigger(UPDATED_HOSPITALMAP_DELETE_TRIGGER)  # type: ignore[attr-defined]
+    op.create_trigger(UPDATED_HOSPITALMAP_INSERT_TRIGGER)  # type: ignore[attr-defined]
+    op.create_trigger(UPDATED_HOSPITALMAP_UPDATE_TRIGGER)  # type: ignore[attr-defined]
 
 
 def downgrade() -> None:
     """Revert QRImageFileName column from hospitalmap and Triggers to remove QRImageFileName instance"""
     op.add_column('HospitalMapMH', sa.Column('QRImageFileName', mysql.VARCHAR(length=255), nullable=False))
     op.add_column('HospitalMap', sa.Column('QRImageFileName', mysql.VARCHAR(length=255), nullable=False))
-    op.drop_trigger(UPDATED_HOSPITALMAP_DELETE_TRIGGER)
-    op.drop_trigger(UPDATED_HOSPITALMAP_INSERT_TRIGGER)
-    op.drop_trigger(UPDATED_HOSPITALMAP_UPDATE_TRIGGER)
-    op.create_trigger(OLD_HOSPITALMAP_DELETE_TRIGGER)
-    op.create_trigger(OLD_HOSPITALMAP_INSERT_TRIGGER)
-    op.create_trigger(OLD_HOSPITALMAP_UPDATE_TRIGGER)
+    op.drop_trigger(UPDATED_HOSPITALMAP_DELETE_TRIGGER)  # type: ignore[attr-defined]
+    op.drop_trigger(UPDATED_HOSPITALMAP_INSERT_TRIGGER)  # type: ignore[attr-defined]
+    op.drop_trigger(UPDATED_HOSPITALMAP_UPDATE_TRIGGER)  # type: ignore[attr-defined]
+    op.create_trigger(OLD_HOSPITALMAP_DELETE_TRIGGER)  # type: ignore[attr-defined]
+    op.create_trigger(OLD_HOSPITALMAP_INSERT_TRIGGER)  # type: ignore[attr-defined]
+    op.create_trigger(OLD_HOSPITALMAP_UPDATE_TRIGGER)  # type: ignore[attr-defined]
