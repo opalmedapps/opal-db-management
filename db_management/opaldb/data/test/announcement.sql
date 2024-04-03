778532c6264adc3007f4ccac50405c9f4b063e16
Stacey Beard <stacey.beard@muhc.mcgill.ca>
2024-04-03 20:39:22 +0000

    fix: update announcement dates and read statuses
    Improved data consistency and demo-readiness for Announcement data.
    
    * Set all announcements to have been sent at different times within the last two weeks (relative to the time of each data refresh).
    * Marked all announcements and their notifications as read for the relevant test accounts.
    
    See merge request opalmedapps/db-docker!301

--- db_management/opaldb/data/test/announcement.sql
+++ db_management/opaldb/data/test/announcement.sql
@@ -1,8 +1,89 @@
 INSERT INTO `Announcement` (`AnnouncementSerNum`, `CronLogSerNum`, `PatientSerNum`, `PostControlSerNum`, `DateAdded`, `ReadStatus`, `ReadBy`, `LastUpdated`) VALUES
-(1,	NULL,	51,	16,	'2023-03-22 00:00:00',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(2,	NULL,	51,	20,	'2023-03-22 00:00:00',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(3,	NULL,	51,	17,	'2023-03-22 00:00:00',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(9,	NULL,	52,	16,	'2023-04-12 00:00:00',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(10,	NULL,	52,	20,	'2023-04-12 00:00:00',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(11,	NULL,	52,	17,	'2023-04-12 00:00:00',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(12,	NULL,	52,	23,	'2023-04-13 00:00:00',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17');
+(1,	NULL,	51,	16,	'2000-01-01 00:00:00',	0,	'[]',	'2000-01-01 00:00:00'),
+(2,	NULL,	51,	20,	'2000-01-01 00:00:00',	0,	'[]',	'2000-01-01 00:00:00'),
+(3,	NULL,	51,	17,	'2000-01-01 00:00:00',	0,	'[]',	'2000-01-01 00:00:00'),
+(9,	NULL,	52,	16,	'2000-01-01 00:00:00',	0,	'[]',	'2000-01-01 00:00:00'),
+(10,	NULL,	52,	20,	'2000-01-01 00:00:00',	0,	'[]',	'2000-01-01 00:00:00'),
+(11,	NULL,	52,	17,	'2000-01-01 00:00:00',	0,	'[]',	'2000-01-01 00:00:00'),
+(12,	NULL,	52,	23,	'2000-01-01 00:00:00',	0,	'[]',	'2000-01-01 00:00:00');
+
+-- Update all Announcement dates and read statuses to make the data more similar to a live environment
+
+-- All Announcements and their Notifications marked as read
+-- Marge's data read by Marge
+UPDATE Announcement
+SET ReadStatus = 1,
+    ReadBy = '["QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2"]'
+WHERE PatientSerNum = 51
+;
+UPDATE Notification
+SET ReadStatus = 1,
+    ReadBy = '["QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2"]'
+WHERE PatientSerNum = 51
+  AND NotificationControlSerNum = 5
+;
+
+-- Homer's data read by Homer and Marge
+UPDATE Announcement
+SET ReadStatus = 1,
+    ReadBy = '["PyKlcbRpMLVm8lVnuopFnFOHO4B3", "QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2"]'
+WHERE PatientSerNum = 52
+;
+UPDATE Notification
+SET ReadStatus = 1,
+    ReadBy = '["PyKlcbRpMLVm8lVnuopFnFOHO4B3", "QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2"]'
+WHERE PatientSerNum = 52
+  AND NotificationControlSerNum = 5
+;
+
+-- "No radiotherapy treatments" sent 1 week ago
+UPDATE Announcement
+SET DateAdded = DATE_ADD(now(), INTERVAL -7 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -7 DAY)
+WHERE PostControlSerNum = 16
+;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -7 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -7 DAY)
+WHERE NotificationControlSerNum = 5
+AND RefTableRowSerNum IN (1, 9)
+;
+
+-- "International Day of Medical Physics" sent 5 days ago
+UPDATE Announcement
+SET DateAdded = DATE_ADD(now(), INTERVAL -5 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -5 DAY)
+WHERE PostControlSerNum = 20
+;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -5 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -5 DAY)
+WHERE NotificationControlSerNum = 5
+AND RefTableRowSerNum IN (2, 10)
+;
+
+-- "Radiation Oncology will be Closed" sent 2 weeks ago
+UPDATE Announcement
+SET DateAdded = DATE_ADD(now(), INTERVAL -14 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -14 DAY)
+WHERE PostControlSerNum = 17
+;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -14 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -14 DAY)
+WHERE NotificationControlSerNum = 5
+AND RefTableRowSerNum IN (3, 11)
+;
+
+-- "Treatment Delay" sent 2 days ago
+UPDATE Announcement
+SET DateAdded = DATE_ADD(now(), INTERVAL -2 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -2 DAY)
+WHERE PostControlSerNum = 23
+;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -2 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -2 DAY)
+WHERE NotificationControlSerNum = 5
+AND RefTableRowSerNum = 12
+;
