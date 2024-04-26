5c6c3cad85cf1057944fb821559d25df453d5bd1
Napparific <kelly.agnew@mail.mcgill.ca>
2024-04-26 11:34:13 -0400

    remove system versioning from orms database

---
+++ db_management/ormsdb/versions/2024_04_26-62f6dbb1514f_drop_system_versioning.py
@@ -0,0 +1,45 @@
+"""Disable system versioning for OrmsDatabase.
+
+Revision ID: 62f6dbb1514f
+Revises: 7b734eb191ab
+Create Date: 2024-04-26 15:26:14.206735
+
+"""
+import os
+from pathlib import Path
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '62f6dbb1514f'
+down_revision = '7b734eb191ab'
+branch_labels = None
+depends_on = None
+
+# Find root and revision data paths for reading sql files
+ROOT_DIR = Path(__file__).parents[1]
+REVISIONS_DIR = ROOT_DIR / 'revision_data'
+
+def upgrade() -> None:
+    ## Disable System Versioning
+    funcs_sql_content = ''
+    funcs_file_path = os.path.join(REVISIONS_DIR, 'OrmsDB_disable_system_versioning.sql')
+    # Read in SQL content from handle
+    with Path(funcs_file_path, encoding='ISO-8859-1').open(encoding='ISO-8859-1') as file_handle:
+        funcs_sql_content += file_handle.read()
+        file_handle.close()
+    op.execute(funcs_sql_content)
+    op.execute('SET foreign_key_checks=1;')
+
+
+def downgrade() -> None:
+    ## Enable System Versioning
+    funcs_sql_content = ''
+    funcs_file_path = os.path.join(REVISIONS_DIR, 'OrmsDB_enable_system_versioning.sql')
+    # Read in SQL content from handle
+    with Path(funcs_file_path, encoding='ISO-8859-1').open(encoding='ISO-8859-1') as file_handle:
+        funcs_sql_content += file_handle.read()
+        file_handle.close()
+    op.execute(funcs_sql_content)
+    op.execute('SET foreign_key_checks=1;')
