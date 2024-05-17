2cc5392e4a1ad50f00ed78d04e77a5a7192594ee
Kelly Agnew <kelly.agnew@mail.mcgill.ca>
2024-05-17 14:33:57 +0000

    fix: various appointment data fixes for checkin troubleshooting
    See merge request opalmedapps/db-docker#333

--- db_management/opaldb/data/test/omi/ori_appointment.sql
+++ db_management/opaldb/data/test/omi/ori_appointment.sql
@@ -1,10 +1,10 @@
 INSERT INTO `Appointment` (`AppointmentSerNum`, `AliasExpressionSerNum`, `CronLogSerNum`, `PatientSerNum`, `SourceDatabaseSerNum`, `AppointmentAriaSer`, `PrioritySerNum`, `DiagnosisSerNum`, `Status`, `State`, `ScheduledStartTime`, `ScheduledEndTime`, `ActualStartDate`, `ActualEndDate`, `Location`, `RoomLocation_EN`, `RoomLocation_FR`, `Checkin`, `CheckinUsername`, `ChangeRequest`, `DateAdded`, `ReadStatus`, `ReadBy`, `SessionId`, `LastUpdated`) VALUES
 
 -- fred
-(219506,	37,	NULL,	56,	2,	219506,	0,	4,	'Open',	'Active',	'2023-04-25 14:00:00',	'2023-04-25 14:10:00',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	10,	'',	'',	0,	'',	0,	'2021-08-25 10:22:33',	0,	'[]',	'',	'2023-01-12 16:39:17'),
+(219506,	37,	NULL,	56,	1,	219506,	0,	4,	'Open',	'Active',	'2023-04-25 14:00:00',	'2023-04-25 14:10:00',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	10,	'',	'',	0,	'',	0,	'2021-08-25 10:22:33',	0,	'[]',	'',	'2023-01-12 16:39:17'),
 
 -- pebbles
-(209499,	24,	NULL,	57,	2,	209499,	0,	5,	'Open',	'Active',	'2023-06-01 17:00:00',	'2023-06-01 17:10:00',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	10,	'',	'',	0,	'',	0,	'2021-06-01 10:46:24',	0,	'[]',	'',	'2023-01-12 16:39:17');
+(209499,	24,	NULL,	57,	1,	209499,	0,	5,	'Open',	'Active',	'2023-06-01 17:00:00',	'2023-06-01 17:10:00',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	10,	'',	'',	0,	'',	0,	'2021-06-01 10:46:24',	0,	'[]',	'',	'2023-01-12 16:39:17');
 
 -- Move fred's to later
 UPDATE Appointment set ScheduledStartTime=concat(current_date(),' ',TIME(DATE_ADD(now(),interval 5 hour))) where PatientSerNum=56;
---
+++ db_management/ormsdb/data/test/16_smsappointment.sql
@@ -0,0 +1,8 @@
+INSERT INTO `SmsAppointment` (`SmsAppointmentId`, `ClinicResourcesSerNum`, `AppointmentCodeId`, `SpecialityGroupId`, `SourceSystem`, `Type`, `Active`, `LastUpdated`) VALUES
+(1, 1312, 516, 1, 'Aria', 'GENERAL', 0, '2024-05-16 07:40:43'),
+(2, 1744, 624, 1, 'Aria', 'GENERAL', 0, '2024-05-16 07:40:43'),
+(3, 1312, 507, 1, 'Aria', 'GENERAL', 0, '2024-05-16 07:40:43'),
+(4, 1312, 501, 1, 'Aria', 'GENERAL', 0, '2024-05-16 07:40:43'),
+(5, 1312, 504, 1, 'Aria', 'GENERAL', 0, '2024-05-16 07:40:43'),
+(6, 1088, 22, 1, 'Aria', 'GENERAL', 0, '2024-05-16 07:40:43'),
+(7, 2360, 105, 1, 'Aria', 'GENERAL', 0, '2024-05-16 07:40:43');
