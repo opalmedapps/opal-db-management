109c0279c3f4790fbc98440a397b760f78f716b9
Napparific <kelly.agnew@mail.mcgill.ca>
2024-05-29 11:44:04 -0400

    identifiers, diag, announcements, documents, actions and activities

--- db_management/opaldb/data/test/omi/omi_01_initial.sql
+++ db_management/opaldb/data/test/omi/omi_01_initial.sql
@@ -11,7 +11,8 @@ INSERT INTO `Patient` (`PatientSerNum`, `PatientAriaSer`, `PatientId`, `PatientI
 (51,	0,	'',	'',	'Marge',	'Simpson',	'marge_test',	NULL,	'Female',	'1986-10-01 00:00:00',	0,	15144758941,	0,	'marge@opalmedapps.ca',	'EN',	'SIMM86600199',	'3',	DATE_ADD(NOW(), INTERVAL -2 MONTH),	'2019-01-01 00:00:00',	0,	'',	'0000-00-00 00:00:00',	'',	'2023-05-25 00:00:00',	1,	1,	DATE_ADD(NOW(), INTERVAL -2 MONTH)),
 (52,	0,	'',	'',	'Homer',	'Simpson',	'homer_test',	NULL,	'Male',	'1983-05-12 00:00:00',	0,	14381234567,	0,	'homer@opalmedapps.ca',	'EN',	'SIMH83051299',	'3',	DATE_ADD(NOW(), INTERVAL -1 MONTH),	'2019-01-01 00:00:00',	1,	'',	'0000-00-00 00:00:00',	'',	'2023-05-25 00:00:00',	1,	1,	DATE_ADD(NOW(), INTERVAL -1 MONTH)),
 (53,	0,	'',	'',	'Bart',	    'Simpson',	'bart_test',	NULL,	'Other',	'2009-02-23 00:00:00',	0,	61292507111,	0,	'bart@opalmedapps.ca',	'EN',	'SIMB13022399',	'3',	DATE_ADD(NOW(), INTERVAL -14 DAY),	'2019-01-01 00:00:00',	0,	'',	'0000-00-00 00:00:00',	'',	'2023-05-25 00:00:00',	1,	1,	DATE_ADD(NOW(), INTERVAL -14 DAY)),
-(55,	0,	'',	'',	'Mona',	    'Simpson',	'mona_test',	NULL,	'Female',	'1940-03-15 00:00:00',	0,	15144758941,	0,	'mona@opalmedapps.ca',	'EN',	'SIMM40531599',	'1',	DATE_ADD(NOW(), INTERVAL -1 YEAR),	'2019-01-01 00:00:00',	1,	'Deceased',	'2021-05-29 00:00:00',	'',	'2023-05-25 00:00:00',	1,	1,	DATE_ADD(NOW(), INTERVAL -1 YEAR));
+(55,	0,	'',	'',	'Mona',	    'Simpson',	'mona_test',	NULL,	'Female',	'1940-03-15 00:00:00',	0,	15144758941,	0,	'mona@opalmedapps.ca',	'EN',	'SIMM40531599',	'1',	DATE_ADD(NOW(), INTERVAL -1 YEAR),	'2019-01-01 00:00:00',	1,	'Deceased',	'2021-05-29 00:00:00',	'',	'2023-05-25 00:00:00',	1,	1,	DATE_ADD(NOW(), INTERVAL -1 YEAR)),
+(92, 43235, '1092300', '5024737', 'Laurie', 'Hendren', 'Pointer Lady', '', 'Female', '1958-12-13 00:00:00', 64, 5144415642, 1, 'hendren@cs.mcgill.ca', 'EN', 'HENL58621319', '3', '2018-01-01 00:00:00', '2019-01-01 00:00:00', 0, 'Unlock by Johns Request', '2019-05-27 00:00:00', '', '2024-04-18 17:59:43', 0, NULL, NULL);
 
 UPDATE `Patient` SET `Age` = DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), `DateOfBirth`)), '%Y') + 0;
 
@@ -19,7 +20,8 @@ INSERT INTO `PatientControl` (`PatientSerNum`, `PatientUpdate`, `LastTransferred
 (51,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0),
 (52,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0),
 (53,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0),
-(55,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0);
+(55,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0),
+(92,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0);
 
 INSERT INTO `Patient_Hospital_Identifier` (`Patient_Hospital_Identifier_Id`, `PatientSerNum`, `Hospital_Identifier_Type_Code`, `MRN`, `Is_Active`) VALUES
 (1,	51,	'RVH',	'9999996',	1),
@@ -28,13 +30,16 @@ INSERT INTO `Patient_Hospital_Identifier` (`Patient_Hospital_Identifier_Id`, `Pa
 (4,	53,	'MCH',	'9999996',	1),
 (6,	55,	'RVH',	'9999993',	1),
 (7,	55,	'MCH',	'5407383',	1),
-(8,	51,	'LAC',	'0389731',	1);
+(8,	51,	'LAC',	'0389731',	1),
+(12, 92, 'MGH',	'5024737',	1),
+(13, 92, 'LAC',	'1092300',	1);
 
 INSERT INTO `Users` (`UserSerNum`, `UserType`, `UserTypeSerNum`, `Username`, `Password`, `SessionId`, `LastUpdated`) VALUES
 (1,	'Patient',	51,	'QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59'),
 (2,	'Patient',	52,	'PyKlcbRpMLVm8lVnuopFnFOHO4B3',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59'),
 (3,	'Patient',	53,	'SipDLZCcOyTYj7O3C8HnWLalb4G3',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59'),
-(4,	'Patient',	55,	'61DXBRwLCmPxlaUoX6M1MP9DiEl1',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59');
+(4,	'Patient',	55,	'61DXBRwLCmPxlaUoX6M1MP9DiEl1',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59'),
+(6,	'Patient',	92,	'a51fba18-3810-4808-9238-4d0e487785c8',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59');
 
 -- We add two sets of Security Answers to test Django's migration scripts, they aren't actually accessed in OpalDB anymore.
 INSERT INTO `SecurityAnswer` (`SecurityQuestionSerNum`, `PatientSerNum`, `AnswerText`, `CreationDate`, `LastUpdated`) VALUES
@@ -74,7 +79,7 @@ INSERT INTO `masterSourceAlias` (`ID`, `externalId`, `code`, `description`, `typ
 INSERT INTO `AliasExpression` (`AliasExpressionSerNum`, `AliasSerNum`, `masterSourceAliasId`, `ExpressionName`, `Description`, `LastTransferred`, `LastUpdatedBy`, `LastUpdated`, `SessionId`) VALUES
 (24, 68, 1110, 'CT Sim (TASKED)', 'CT Sim (TASKED)', '2022-01-21 17:58:01', 3, '2022-01-21 20:45:31', 'LMVcPrElSW'),
 (37, 6, 173, '.EBC-Daily Rx', '.EBC-Daily Rx', '2022-01-21 17:58:01', NULL, '2022-01-21 20:45:28', NULL),
-(99,	12,	NULL,	'RO - Consult',	'RO - Consult',	'2021-09-30 00:56:01',	NULL,	'2018-10-19 23:55:39',	NULL),
+(99,	12,	3679,	'RO - Consult',	'RO - Consult',	'2021-09-30 00:56:01',	NULL,	'2018-10-19 23:55:39',	NULL),
 (147, 61, 1347, 'FOLLOW UP IN LESS/30DAYS', 'FOLLOW UP IN LESS/30DAYS', '2022-01-21 17:58:01', 3, '2022-01-21 20:45:32', 'JGjfgEuYTa'),
 (174, 49, 1035, 'CONSULT NEW IN', 'CONSULT NEW IN', '2022-01-21 17:58:01', 3, '2022-01-21 20:45:31', 'I6wD87Yph8'),
 (8114, 382, 1233, 'INTRA TREAT OUT', 'INTRA TREAT OUT', '2022-01-21 17:58:01', 3, '2022-01-21 20:45:32', 'SUlHb39TI7'),
