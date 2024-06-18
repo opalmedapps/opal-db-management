de4fc1c46aeed83e42be9aa3f1f6ce7d8b810f93
Matthias Schoettle <3097098-mschoettle@users.noreply.gitlab.com>
2024-06-18 15:08:41 +0000

    fix: update respondent for answered questionnaires when upgrading
    See merge request opalmedapps/db-docker!351

--- db_management/questionnairedb/data/upgrade/2024.sql
+++ db_management/questionnairedb/data/upgrade/2024.sql
@@ -1,4 +1,4 @@
--- Upgrade script to upgrade from the latest version (January 2022) at the MUHC to the new version (TBD in 2023)
+-- Upgrade script to upgrade from the latest version (January 2022) at the MUHC to the new version (TBD in 2024)
 
 -- Insert alembic tracking table
 CREATE TABLE `alembic_version` (
@@ -7,3 +7,17 @@ CREATE TABLE `alembic_version` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 INSERT INTO `alembic_version` (`version_num`) VALUES
 ('a8a23f24d61b');
+
+-- Update respondentDisplayName for completed and in progress questionnaires
+UPDATE answerQuestionnaire AS aq
+JOIN patient AS p ON aq.patientId = p.ID
+JOIN OpalDB.Patient AS op ON op.PatientSerNum = p.externalId
+SET aq.respondentDisplayName = CONCAT_WS(' ', op.FirstName, op.LastName)
+WHERE aq.Status = 1 OR aq.Status = 2;
+
+-- Update respondentUsername for completed and in progress questionnaires
+UPDATE answerQuestionnaire AS aq
+JOIN patient AS p ON aq.patientId = p.ID
+JOIN OpalDB.Users AS ou ON ou.UserTypeSerNum = p.externalId
+SET aq.respondentUsername = ou.Username
+WHERE aq.Status = 1 OR aq.Status = 2;
