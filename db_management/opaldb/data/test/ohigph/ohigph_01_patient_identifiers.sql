637fcad39272f09564da6ff4f052068525b279ec
Matthias Schoettle <3097098-mschoettle@users.noreply.gitlab.com>
2024-08-02 18:41:53 +0000

    fix(data): fix OHIGPH user ID
    See merge request opalmedapps/db-docker!367

--- db_management/opaldb/data/test/ohigph/ohigph_01_patient_identifiers.sql
+++ db_management/opaldb/data/test/ohigph/ohigph_01_patient_identifiers.sql
@@ -20,7 +20,7 @@ INSERT INTO `Patient_Hospital_Identifier` (`Patient_Hospital_Identifier_Id`, `Pa
 
 INSERT INTO `Users` (`UserSerNum`, `UserType`, `UserTypeSerNum`, `Username`, `Password`, `SessionId`, `LastUpdated`) VALUES
 (1,	'Caregiver',	51,	'QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59'),
-(2,	'Patient',	    53,	'SipDLZCcOyTYj7O3C8HnWLalb4G3',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59');
+(3,	'Patient',	    53,	'SipDLZCcOyTYj7O3C8HnWLalb4G3',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59');
 
 
 -- We add one set of Security Answers to test Django's migration scripts, they aren't actually accessed in OpalDB anymore.
