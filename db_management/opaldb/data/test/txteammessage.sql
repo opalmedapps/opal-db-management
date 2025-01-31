bf03f10e2aee0227a456d31f3c2288d46a879f8d
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 19:14:53 +0000

    fix: reorder updates so the LastUpdated values aren't overwritten
    (cherry picked from commit 1e44ccdcb3b24f4daa0766ebf3d8494d6d029de0)
    
    Co-authored-by: Stacey Beard <staceybeard10@gmail.com>

--- db_management/opaldb/data/test/txteammessage.sql
+++ db_management/opaldb/data/test/txteammessage.sql
@@ -3,20 +3,7 @@ INSERT INTO `TxTeamMessage` (`TxTeamMessageSerNum`, `CronLogSerNum`, `PatientSer
 (2,	NULL,	52,	13,	'2000-01-01 00:00:00',	0,	'[]',	'2000-01-01 00:00:00'),
 (3,	NULL,	53,	13,	'2000-01-01 00:00:00',	0,	'[]',	'2000-01-01 00:00:00');
 
--- Update all TxTeamMessage dates to make the data more similar to a live environment
-
--- Welcome Message sent 4 weeks ago
-UPDATE TxTeamMessage
-SET DateAdded = DATE_ADD(now(), INTERVAL -28 DAY),
-    LastUpdated = DATE_ADD(now(), INTERVAL -28 DAY)
-WHERE PostControlSerNum = 13
-;
-UPDATE Notification
-SET DateAdded = DATE_ADD(now(), INTERVAL -28 DAY),
-    LastUpdated = DATE_ADD(now(), INTERVAL -28 DAY)
-WHERE NotificationControlSerNum = 4
-AND RefTableRowSerNum IN (1, 2, 3)
-;
+-- Update all TxTeamMessage dates and read statuses to make the data more similar to a live environment
 
 -- All TxTeamMessages and their Notifications marked as read
 -- Marge's data read by Marge
@@ -56,4 +43,17 @@ SET ReadStatus = 1,
     ReadBy = '["SipDLZCcOyTYj7O3C8HnWLalb4G3", "QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2"]'
 WHERE PatientSerNum = 53
   AND NotificationControlSerNum = 4
+;
+
+-- Welcome Message sent 4 weeks ago
+UPDATE TxTeamMessage
+SET DateAdded = DATE_ADD(now(), INTERVAL -28 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -28 DAY)
+WHERE PostControlSerNum = 13
+;
+UPDATE Notification
+SET DateAdded = DATE_ADD(now(), INTERVAL -28 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -28 DAY)
+WHERE NotificationControlSerNum = 4
+AND RefTableRowSerNum IN (1, 2, 3)
 ;
