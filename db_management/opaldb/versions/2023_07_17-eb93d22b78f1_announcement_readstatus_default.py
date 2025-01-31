df1d8bc48ae0a12582b7a12ef00cea5c863714d9
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 19:02:03 +0000

    fix commit script file
    (cherry picked from commit b1a452c276522e384a05d8bfecf90ccd64299339)
    
    Co-authored-by: LiminLiu <Limin.Liu@muhc.mcgill.ca>

---
+++ db_management/opaldb/versions/2023_07_17-eb93d22b78f1_announcement_readstatus_default.py
@@ -0,0 +1,36 @@
+"""‘Announcement_readStatus_default’
+
+Revision ID: eb93d22b78f1
+Revises: 759547f5ca7d
+Create Date: 2023-07-17 18:22:23.476788
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision = 'eb93d22b78f1'
+down_revision = '759547f5ca7d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # ### Add default value for the field ReadStatus ###
+    op.alter_column('Announcement', 'ReadStatus',
+               existing_type=mysql.INTEGER(display_width=11),
+               server_default=sa.text('0'),
+               existing_comment='Deprecated',
+               existing_nullable=False)
+    # ### end Alembic commands ###
+
+
+def downgrade() -> None:
+    # ### Removed the default value for the field ReasStatus ###
+    op.alter_column('Announcement', 'ReadStatus',
+               existing_type=mysql.INTEGER(display_width=11),
+               server_default=None,
+               existing_comment='Deprecated',
+               existing_nullable=False)
+    # ### end Alembic commands ###
