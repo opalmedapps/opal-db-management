06ed71cc5e2426fb82cc77a889dae46b3bac086d
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 19:09:52 +0000

    ci: use pytest-alembic instead
    (cherry picked from commit 62ab7d42bc727e45d8f317732c11c1319ad3d3ec)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

---
+++ test_alembic.py
@@ -0,0 +1,34 @@
+import pytest
+
+from pytest_alembic import Config
+
+from pytest_alembic import Config, create_alembic_fixture, tests
+
+opaldb = create_alembic_fixture({'script_location': 'db_management/opaldb'})
+questionnairedb = create_alembic_fixture({'script_location': 'db_management/questionnairedb'})
+
+def test_opaldb_single_head_revision(opaldb):
+    tests.test_single_head_revision(opaldb)
+
+def test_opaldb_upgrade(opaldb):
+    tests.test_upgrade(opaldb)
+
+def test_opaldb_model_definitions_match_ddl(opaldb):
+    # print(opaldb.config)
+    # assert False
+    tests.test_model_definitions_match_ddl(opaldb)
+
+# def test_opaldb_up_down_consistency(opaldb):
+    # tests.test_up_down_consistency(opaldb)
+
+# def test_questionnairedb_single_head_revision(questionnairedb):
+#     tests.test_single_head_revision(questionnairedb)
+
+# def test_questionnairedb_upgrade(questionnairedb):
+#     tests.test_upgrade(questionnairedb)
+
+# def test_questionnairedb_model_definitions_match_ddl(questionnairedb):
+#     tests.test_model_definitions_match_ddl(questionnairedb)
+
+# def test_questionnairedb_up_down_consistency(questionnairedb):
+#     tests.test_up_down_consistency(questionnairedb)
