b11c27ed1573dcb5b96a73e00e56e4a4d50a7a68
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 19:18:44 +0000

    fix questionnaire status
    (cherry picked from commit 7e24507083e700ecaf05fb643d0744ecd1bd09a0)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

--- db_management/questionnairedb/data/upgrade/2024.sql
+++ db_management/questionnairedb/data/upgrade/2024.sql
@@ -8,16 +8,16 @@ CREATE TABLE `alembic_version` (
 INSERT INTO `alembic_version` (`version_num`) VALUES
 ('a8a23f24d61b');
 
--- Update respondentDisplayName
+-- Update respondentDisplayName for completed and in progress questionnaires
 UPDATE answerQuestionnaire AS aq
 JOIN patient AS p ON aq.patientId = p.ID
 JOIN OpalDB.Patient AS op ON op.PatientSerNum = p.externalId
 SET aq.respondentDisplayName = CONCAT_WS(' ', op.FirstName, op.LastName)
-WHERE aq.Status = 2 OR aq.Status = 3;
+WHERE aq.Status = 1 OR aq.Status = 2;
 
--- Update respondentUsername
+-- Update respondentUsername for completed and in progress questionnaires
 UPDATE answerQuestionnaire AS aq
 JOIN patient AS p ON aq.patientId = p.ID
 JOIN OpalDB.Users AS ou ON ou.UserTypeSerNum = p.externalId
 SET aq.respondentUsername = ou.Username
-WHERE aq.Status = 2 OR aq.Status = 3;
+WHERE aq.Status = 1 OR aq.Status = 2;
