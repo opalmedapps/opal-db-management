f3671bd26cfe1f17cef9ceb1e1469ffc148a771f
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 19:04:12 +0000

    fix: remove readby content
    (cherry picked from commit 2142eec12bbf9bf0e78dc7229aa9779bb43570fb)
    
    Co-authored-by: user.email <asaig@hulool.com>

--- db_management/opaldb/data/test/chusj/chusj.sql
+++ db_management/opaldb/data/test/chusj/chusj.sql
@@ -28,12 +28,13 @@ INSERT INTO `SecurityAnswer` (`SecurityQuestionSerNum`, `PatientSerNum`, `Answer
 (5,	53,	'a7dbabba9a0371fbdb92724a5ca66401e02069089b1f3a100374e61f934fe9e959215ae0327de2bc064a9dfc351c4d64ef89bd47e95be0198a1f466c3518cc1d',	'2018-11-08 12:38:47',	'2019-01-11 02:45:32');
 
 -- We add those records here to manually insert `Clinical Notes` documents  for hospital-specific `chusj` demo purposes
+-- ReadBy field can be added to the list separated by comma e.g. [\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]
 -- Bart
 INSERT INTO `Document` (`DocumentSerNum`, `CronLogSerNum`, `PatientSerNum`, `SourceDatabaseSerNum`, `DocumentId`, `AliasExpressionSerNum`, `ApprovedBySerNum`, `ApprovedTimeStamp`, `AuthoredBySerNum`, `DateOfService`, `Revised`, `ValidEntry`, `ErrorReasonText`, `OriginalFileName`, `FinalFileName`, `CreatedBySerNum`, `CreatedTimeStamp`, `TransferStatus`, `TransferLog`, `SessionId`, `DateAdded`, `ReadStatus`, `ReadBy`, `LastUpdated`) VALUES
-(5,	NULL,	53,	1,	'56190000000000039165511',	99,	890,	'2023-06-01 12:36:00',	890,	'2023-06-08 12:35:00',	'',	'Y',	'',	'bart_2009Feb23_note.pdf',	'bart_2009Feb23_note.pdf',	890,	'2023-06-08 12:36:00',	'T',	'Transfer successful',	'',	'2023-06-09 16:38:26',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17');
+(5,	NULL,	53,	1,	'56190000000000039165511',	99,	890,	'2023-06-01 12:36:00',	890,	'2023-06-08 12:35:00',	'',	'Y',	'',	'bart_2009Feb23_note.pdf',	'bart_2009Feb23_note.pdf',	890,	'2023-06-08 12:36:00',	'T',	'Transfer successful',	'',	'2023-06-09 16:38:26',	1,	'[]',	'2023-01-12 16:39:17');
 -- Lisa
 INSERT INTO `Document` (`DocumentSerNum`, `CronLogSerNum`, `PatientSerNum`, `SourceDatabaseSerNum`, `DocumentId`, `AliasExpressionSerNum`, `ApprovedBySerNum`, `ApprovedTimeStamp`, `AuthoredBySerNum`, `DateOfService`, `Revised`, `ValidEntry`, `ErrorReasonText`, `OriginalFileName`, `FinalFileName`, `CreatedBySerNum`, `CreatedTimeStamp`, `TransferStatus`, `TransferLog`, `SessionId`, `DateAdded`, `ReadStatus`, `ReadBy`, `LastUpdated`) VALUES
-(6,	NULL,	54,	1,	'56190000000000039165512',	99,	890,	'2023-06-01 12:36:00',	890,	'2023-06-08 12:35:00',	'',	'Y',	'',	'lisa_2014May09_note.pdf',	'lisa_2014May09_note.pdf',	890,	'2023-06-08 12:36:00',	'T',	'Transfer successful',	'',	'2023-06-09 16:38:26',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17');
+(6,	NULL,	54,	1,	'56190000000000039165512',	99,	890,	'2023-06-01 12:36:00',	890,	'2023-06-08 12:35:00',	'',	'Y',	'',	'lisa_2014May09_note.pdf',	'lisa_2014May09_note.pdf',	890,	'2023-06-08 12:36:00',	'T',	'Transfer successful',	'',	'2023-06-09 16:38:26',	1,	'[]',	'2023-01-12 16:39:17');
 
 -- We insert staff that are defined in the documents added in the `Document` insert query above to make sure
 -- the doctor referred to in the document exists in the test data
