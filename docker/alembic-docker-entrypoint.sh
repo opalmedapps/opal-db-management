f345a564d489017fa5f8f754873b3db8fb82de37
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 18:59:29 +0000

    feat: add main ormsdb schema to alembic
    (cherry picked from commit 93b843ee3765b5e6f50dac0a4d49b3f86e092d0e)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- docker/alembic-docker-entrypoint.sh
+++ docker/alembic-docker-entrypoint.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 # Note: Due to foreign key constraint OpalDB must be after QuestionnaireDB
 alembic --name questionnairedb upgrade head
 alembic --name opaldb upgrade head
+alembic --name ormsdb upgrade head
 
 exec "$@"
