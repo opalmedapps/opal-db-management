56663c95be1a3c957c8cb2215ce8c75f59dfcaa1
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 18:56:56 +0000

    Merge branch 'main' into QSCCD-1085.SF.save-username-submitting-educational-materials-rating
    (cherry picked from commit 93733c7d52a020a12956cd50ca5671ffffb66648)
    
    Co-authored-by: Shifeng Chen <shifeng.chen@muhc.mcgill.ca>

---
+++ alembic/opaldb/migrations/versions/bcf300b3df7b_add_missing_defaults.py
@@ -0,0 +1,125 @@
+"""add_missing_defaults
+
+Revision ID: bcf300b3df7b
+Revises: 9f32d05833a5
+Create Date: 2023-05-15 20:35:17.462142
+
+"""
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'bcf300b3df7b'
+down_revision = '9f32d05833a5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Alter existing columns in Questionnaire, QuestionnaireControl, and TxTeamMessages to have proper defaults."""
+    op.alter_column(
+        'PatientTestResult',
+        'ReadStatus',
+        existing_type=mysql.INTEGER(display_width=11),
+        server_default=sa.text('0'),
+        existing_comment='Deprecated',
+        existing_nullable=False,
+    )
+    op.alter_column(
+        'Questionnaire',
+        'DateAdded',
+        existing_type=mysql.DATETIME(),
+        server_default=sa.text('current_timestamp()'),
+        existing_nullable=False,
+    )
+    op.alter_column(
+        'Questionnaire',
+        'CompletedFlag',
+        existing_type=mysql.TINYINT(display_width=4),
+        server_default=sa.text('0'),
+        existing_nullable=False,
+    )
+    op.alter_column(
+        'Questionnaire',
+        'SessionId',
+        existing_type=mysql.TEXT(),
+        server_default=sa.text("'[]'"),
+        existing_nullable=False,
+    )
+    op.alter_column(
+        'QuestionnaireControl',
+        'PublishFlag',
+        existing_type=mysql.TINYINT(display_width=4),
+        server_default=sa.text('0'),
+        existing_nullable=False,
+    )
+    op.alter_column(
+        'QuestionnaireControl',
+        'LastPublished',
+        existing_type=mysql.DATETIME(),
+        nullable=True,
+    )
+    op.alter_column(
+        'TxTeamMessage',
+        'ReadStatus',
+        existing_type=mysql.INTEGER(display_width=11),
+        server_default=sa.text('0'),
+        existing_comment='Deprecated',
+        existing_nullable=False,
+    )
+
+
+def downgrade() -> None:
+    """Revert the above changes."""
+    op.alter_column(
+        'TxTeamMessage',
+        'ReadStatus',
+        existing_type=mysql.INTEGER(display_width=11),
+        server_default=None,
+        existing_comment='Deprecated',
+        existing_nullable=False,
+    )
+    op.alter_column(
+        'QuestionnaireControl',
+        'LastPublished',
+        existing_type=mysql.DATETIME(),
+        nullable=False,
+    )
+    op.alter_column(
+        'QuestionnaireControl',
+        'PublishFlag',
+        existing_type=mysql.TINYINT(display_width=4),
+        server_default=None,
+        existing_nullable=False,
+    )
+    op.alter_column(
+        'Questionnaire',
+        'SessionId',
+        existing_type=mysql.TEXT(),
+        server_default=None,
+        existing_nullable=False,
+    )
+    op.alter_column(
+        'Questionnaire',
+        'CompletedFlag',
+        existing_type=mysql.TINYINT(display_width=4),
+        server_default=None,
+        existing_nullable=False,
+    )
+    op.alter_column(
+        'Questionnaire',
+        'DateAdded',
+        existing_type=mysql.DATETIME(),
+        server_default=None,
+        existing_nullable=False,
+    )
+    op.alter_column(
+        'PatientTestResult',
+        'ReadStatus',
+        existing_type=mysql.INTEGER(display_width=11),
+        server_default=None,
+        existing_comment='Deprecated',
+        existing_nullable=False,
+    )
