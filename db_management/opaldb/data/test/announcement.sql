8244f717eb278d6b528824de12dc3a8f580f1a19
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 19:15:14 +0000

    fix: change announcement ordering
    (cherry picked from commit fbd9fa47ee1504be552b8f38b87245d5074e6195)
    
    Co-authored-by: Stacey Beard <staceybeard10@gmail.com>

--- db_management/opaldb/data/test/announcement.sql
+++ db_management/opaldb/data/test/announcement.sql
@@ -36,41 +36,41 @@ WHERE PatientSerNum = 52
   AND NotificationControlSerNum = 5
 ;
 
--- "No radiotherapy treatments" sent 2 weeks ago
+-- "No radiotherapy treatments" sent 1 week ago
 UPDATE Announcement
-SET DateAdded = DATE_ADD(now(), INTERVAL -14 DAY),
-    LastUpdated = DATE_ADD(now(), INTERVAL -14 DAY)
+SET DateAdded = DATE_ADD(now(), INTERVAL -7 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -7 DAY)
 WHERE PostControlSerNum = 16
 ;
 UPDATE Notification
-SET DateAdded = DATE_ADD(now(), INTERVAL -14 DAY),
-    LastUpdated = DATE_ADD(now(), INTERVAL -14 DAY)
+SET DateAdded = DATE_ADD(now(), INTERVAL -7 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -7 DAY)
 WHERE NotificationControlSerNum = 5
 AND RefTableRowSerNum IN (1, 9)
 ;
 
--- "International Day of Medical Physics" sent 1 week ago
+-- "International Day of Medical Physics" sent 5 days ago
 UPDATE Announcement
-SET DateAdded = DATE_ADD(now(), INTERVAL -7 DAY),
-    LastUpdated = DATE_ADD(now(), INTERVAL -7 DAY)
+SET DateAdded = DATE_ADD(now(), INTERVAL -5 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -5 DAY)
 WHERE PostControlSerNum = 20
 ;
 UPDATE Notification
-SET DateAdded = DATE_ADD(now(), INTERVAL -7 DAY),
-    LastUpdated = DATE_ADD(now(), INTERVAL -7 DAY)
+SET DateAdded = DATE_ADD(now(), INTERVAL -5 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -5 DAY)
 WHERE NotificationControlSerNum = 5
 AND RefTableRowSerNum IN (2, 10)
 ;
 
--- "Radiation Oncology will be Closed" sent 5 days ago
+-- "Radiation Oncology will be Closed" sent 2 weeks ago
 UPDATE Announcement
-SET DateAdded = DATE_ADD(now(), INTERVAL -5 DAY),
-    LastUpdated = DATE_ADD(now(), INTERVAL -5 DAY)
+SET DateAdded = DATE_ADD(now(), INTERVAL -14 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -14 DAY)
 WHERE PostControlSerNum = 17
 ;
 UPDATE Notification
-SET DateAdded = DATE_ADD(now(), INTERVAL -5 DAY),
-    LastUpdated = DATE_ADD(now(), INTERVAL -5 DAY)
+SET DateAdded = DATE_ADD(now(), INTERVAL -14 DAY),
+    LastUpdated = DATE_ADD(now(), INTERVAL -14 DAY)
 WHERE NotificationControlSerNum = 5
 AND RefTableRowSerNum IN (3, 11)
 ;
