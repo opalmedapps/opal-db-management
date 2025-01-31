e71ecd0b49ec1b972fcad8f17e03ae8171ab3e05
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 19:04:30 +0000

    add reset_data command to bundle all data reset commands
    (cherry picked from commit 46574f376a62c3b029ab4a394a95ef97c779f0a3)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

---
+++ db_management/reset_data.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Description: This script resets the test data insertions for OpalDB, QuestionnaireDB, and orms.
+# Args:
+#      Institution: muhc or chusj
+
+
+# Define a log function
+log() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
+}
+
+log "Beginning test data reset."
+
+# Check for the institution argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <institution>"
+    echo "Valid arguments: muhc, chusj"
+    exit 1
+fi
+
+institution=$1
+
+# Validate the institution
+if [[ "$institution" != "muhc" && "$institution" != "chusj" ]]; then
+    echo "Invalid argument: $institution"
+    echo "Valid arguments: muhc, chusj"
+    exit 1
+fi
+
+
+# Define the commands as an array
+declare -a commands=(
+"python -m db_management.run_sql_scripts OpalDB db_management/opaldb/data/truncate/"
+"python -m db_management.run_sql_scripts QuestionnaireDB db_management/questionnairedb/data/truncate/"
+"python -m db_management.run_sql_scripts orms db_management/ormsdb/data/truncate/"
+"python -m db_management.run_sql_scripts OpalDB db_management/opaldb/data/initial/"
+"python -m db_management.run_sql_scripts OpalDB db_management/opaldb/data/test/$institution/"
+"python -m db_management.run_sql_scripts OpalDB db_management/opaldb/data/test/ --disable-foreign-key-checks"
+"python -m db_management.run_sql_scripts QuestionnaireDB db_management/questionnairedb/data/initial/"
+"python -m db_management.run_sql_scripts QuestionnaireDB db_management/questionnairedb/data/test/"
+"python -m db_management.run_sql_scripts orms db_management/ormsdb/data/initial/"
+"python -m db_management.run_sql_scripts orms db_management/ormsdb/data/test/"
+)
+
+
+# Execute each command
+for cmd in "${commands[@]}"; do
+    log "Executing: $cmd"
+    eval "$cmd"
+    if [ $? -eq 0 ]; then
+        log "Command executed successfully."
+    else
+        log "Error executing command. Exiting."
+        exit 1
+    fi
+    echo "------------------------------------------------------------"
+done
+
+log "Test data successfully reset."
