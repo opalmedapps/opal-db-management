76952c84bef44c1d8aa09cc64b37917c04eec2c2
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 19:00:38 +0000

    fix
    (cherry picked from commit f33889f3e1bfb51b44da1285557d67c8b26caff6)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- db_management/opaldb/data/test/educationmaterial.sql
+++ db_management/opaldb/data/test/educationmaterial.sql
@@ -4,7 +4,7 @@ INSERT INTO `EducationalMaterial` (`EducationalMaterialSerNum`, `CronLogSerNum`,
 -- all get: treatment guidelines,
 (1,	NULL,	105,	51,	'2023-02-05 08:00:55',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
 (2,	NULL,	105,	52,	'2023-03-14 08:00:55',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
-(3,	NULL,	105,	53,	'2023-06-05 08:00:55',	0,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
+(3,	NULL,	105,	53,	'2023-05-15 08:00:55',	0,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
 
 -- marge: intro to breast cancer, covid care guide,fertility preservation for women
 (4,	NULL,	960,	51,	'2023-02-15 16:31:26',	1,	'[\"1zdi45eWjPhc0lHRyjMtVV9gGhH3\"]',	'2023-01-12 16:39:17'),
