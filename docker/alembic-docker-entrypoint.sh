e6d199221f95e86c83f775e82260070465e8fd85
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 19:04:23 +0000

    add outputs
    (cherry picked from commit 5fb5f00ed1bd8d148d690870913e75d8f1f04524)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

--- docker/alembic-docker-entrypoint.sh
+++ docker/alembic-docker-entrypoint.sh
@@ -3,9 +3,13 @@ set -euo pipefail
 
 # Alembic populates database according to version files
 # Note: Due to foreign key constraint OpalDB must be after QuestionnaireDB
+echo "Upgrading QuestionnaireDB..."
 alembic --name questionnairedb upgrade head
+echo "Upgrading OpalDB..."
 alembic --name opaldb upgrade head
+echo "Upgrading ormsDB..."
 alembic --name ormsdb upgrade head
+echo "Upgrading ormslogDB..."
 alembic --name ormslogdb upgrade head
 
 exec "$@"
