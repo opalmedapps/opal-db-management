993162ece9a53261b4175bcfd673d44203615492
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-01-31 18:52:25 +0000

    build: use new ssh build option in docker compose
    (cherry picked from commit ed3ddc3d382cd4016c590275f297c05882eb89e5)
    
    Co-authored-by: Matthias Schoettle <matthias.schoettle@muhc.mcgill.ca>

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 
 <h1>DBV Dashboards</h1>
 
-<p><a href="http://localhost:<?php echo getenv("PHP_PORT") ?>/dbv/dbv_opaldb/">Opal DB</a></p>
-<p><a href="http://localhost:<?php echo getenv("PHP_PORT") ?>/dbv/dbv_questionnairedb/">Questionnaire DB</a></p>
-<p><a href="http://localhost:<?php echo getenv("PHP_PORT") ?>/dbv/dbv_registerdb/">Register DB</a></p>
-<p><a href="http://localhost:<?php echo getenv("PHP_PORT") ?>/dbv/dbv_opalreportdb/">Opal Report DB</a></p>
+<p><a href="dbv/dbv_opaldb/">Opal DB</a></p>
+<p><a href="dbv/dbv_questionnairedb/">Questionnaire DB</a></p>
+<p><a href="dbv/dbv_registerdb/">Register DB</a></p>
+<p><a href="dbv/dbv_opalreportdb/">Opal Report DB</a></p>
 
 </body>
 </html>
