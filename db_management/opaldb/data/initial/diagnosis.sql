opalmedapps/opal-db-management

commit de4190f91de70540024016d0b28c3852c413fa43

fix: update demo data for openEMR (#470)
* change user data and add new openemr test data

* remove blink line

* fix master source

db_management/opaldb/data/initial/diagnosis.sql
@@ -49,7 +49,8 @@ INSERT INTO `DiagnosisTranslation` (`DiagnosisTranslationSerNum`, `AliasName`, `
 (1158,	'',	NULL,	'Sarcoma - Upper limb',	'Sarcome - membre supérieur',	'<p>This is a diagnosis of upper limb sarcoma.</p>',	'<p>Ceci est un diagnostic de sarcome des membres supérieurs.</p>',	'',	'2020-02-18 19:14:04',	NULL,	'2020-02-19 05:16:57',	'7CigzsWZsM'),
 (1159,	'',	NULL,	'Sarcoma - Lower limb',	'Sarcome - membre inférieur',	'<p><span style=\"font-size: 14px;float: none;\">This is a diagnosis of upper limb sarcoma.</span><!--EndFragment--><br/><br/></p>',	'<p>Ceci est un diagnostique de sarcome des membres inférieurs.</p>',	'',	'2020-02-18 19:16:11',	NULL,	'2020-02-19 05:17:36',	'7CigzsWZsM'),
 (1160,	'',	NULL,	'Kaposi\'s sarcoma',	'Maladie de Kaposi',	'<p>This is a diagnosis of Kaposi\'s sarcoma.</p>',	'<p>Ceci est un diagnostic de la maladie de Kaposi.</p>',	'',	'2020-02-18 19:19:41',	NULL,	'2020-02-19 05:19:41',	'7CigzsWZsM'),
-(1161,	'',	NULL,	'Hematuria',	'Hématurie',	'<p>A disorder characterized by laboratory test results that indicate blood in the urine.</p>',	'<p>Trouble caractérisé par des résultats de tests de laboratoire indiquant la présence de sang dans l\'urine.</p>',	'',	'2024-05-10 19:16:11',	NULL,	'2024-05-10 05:17:36',	NULL);
+(1161,	'',	NULL,	'Hematuria',	'Hématurie',	'<p>A disorder characterized by laboratory test results that indicate blood in the urine.</p>',	"<p>Trouble caractérisé par des résultats de tests de laboratoire indiquant la présence de sang dans l\'urine.</p>",	'',	'2024-05-10 19:16:11',	NULL,	'2024-05-10 05:17:36',	NULL),
+(1162,  '', NULL,   'Syringomyelia', 'Syringomyelie', '<p>Syringomyelia<br/></p>', '<p>Syringomyelie<br/></p>', '', '2025-02-27 11:14:06', 25, '2025-02-27 11:14:06', '28U2zEskKtB');
 
 INSERT INTO `DiagnosisCode` (`DiagnosisCodeSerNum`, `DiagnosisTranslationSerNum`, `SourceUID`, `Source`, `DiagnosisCode`, `Description`, `DateAdded`, `LastUpdatedBy`, `LastUpdated`, `SessionId`) VALUES
 (228,	1131,	-9173,	-1,	'C33',	'Ca of trachea',	'2018-01-12 14:29:58',	NULL,	'2023-01-12 16:39:12',	NULL),
@@ -1346,10 +1347,10 @@ INSERT INTO `DiagnosisCode` (`DiagnosisCodeSerNum`, `DiagnosisTranslationSerNum`
 (2819,	1129,	-2647,	-1,	'C50.6r',	'Axillary tail of breast',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2820,	1129,	-33037,	-1,	'C50.8',	'Overlapping Ca of  breast (RIGHT)',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2821,	1129,	-21708,	-1,	'C50.8',	'Overlapping Ca of  breast (Right, 2013)',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
-(2822,	1129,	-19470,	-1,	'C50.8',	'Overlapping Ca of  breast a 3 o\'clock',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
-(2823,	1129,	-19475,	-1,	'C50.8',	'Overlapping Ca of  breast at 12 o\'clock',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
-(2824,	1129,	-19473,	-1,	'C50.8',	'Overlapping Ca of  breast at 9 o\'clock',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
-(2825,	1129,	-19696,	-1,	'C50.8',	'Overlapping Ca of  breast, at 12 o\'clock',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
+(2822,	1129,	-19470,	-1,	'C50.8',	"Overlapping Ca of  breast a 3 o\'clock",	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
+(2823,	1129,	-19475,	-1,	'C50.8',	"Overlapping Ca of  breast at 12 o\'clock",	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
+(2824,	1129,	-19473,	-1,	'C50.8',	"Overlapping Ca of  breast at 9 o\'clock",	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
+(2825,	1129,	-19696,	-1,	'C50.8',	"Overlapping Ca of  breast, at 12 o\'clock",	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2826,	1129,	-86,	-1,	'C50.8',	'Overlapping lesion of breast',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2827,	1129,	-1687,	-1,	'C50.8',	'Overlapping lesion of breast (LEFT)',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2828,	1129,	-13214,	-1,	'C50.8',	'Overlapping Ca of  breast (Right)',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
@@ -1357,7 +1358,7 @@ INSERT INTO `DiagnosisCode` (`DiagnosisCodeSerNum`, `DiagnosisTranslationSerNum`
 (2830,	1129,	-9723,	-1,	'C50.8',	'bilateral breast cancer in 2003. She received neoadjuvant chemotherapy for cT2Nx disease on the left and cT3Nx on the right. Following bilateral mastectomy and right-sided nodal dissection, adjuvant radiotherapy was given. This consisted of whole-breast',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2831,	1129,	-8648,	-1,	'C50.8',	'Overlapping Ca of  breast',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2832,	1129,	-16054,	-1,	'C50.8',	'Overlapping Ca of  breast  (LEFT)',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
-(2833,	1129,	-20333,	-1,	'C50.8',	'Overlapping Ca of  breast (3-4 O\'clock)',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
+(2833,	1129,	-20333,	-1,	'C50.8',	"Overlapping Ca of  breast (3-4 O\'clock)",	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2834,	1129,	-11688,	-1,	'C50.8',	'Overlapping Ca of  breast (left)',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2835,	1129,	-11781,	-1,	'C50.8',	'Overlapping Ca of  breast (Left)',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2836,	1129,	-11797,	-1,	'C50.8',	'Overlapping Ca of  breast (left, 2006)',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
@@ -1486,4 +1487,7 @@ INSERT INTO `DiagnosisCode` (`DiagnosisCodeSerNum`, `DiagnosisTranslationSerNum`
 (2959,	1129,	-11187,	-1,	'D24',	'Benign neoplasm of breast',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2960,	1129,	-11430,	-1,	'inflammatory bre',	'Ca of upper-outer quadrant of breast',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2961,	1129,	-9106,	-1,	'T1c N0 (0/6)M0',	'Ca of breast, unspecified',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
-(2962,	1161,	-11431,	-1,	'R31.9',	'Hematuria, unspecified',	'2024-05-10 13:59:13',	NULL,	'2024-05-10 16:39:12',	NULL);
+(2962,	1161,	-11431,	-1,	'R31.9',	'Hematuria, unspecified',	'2024-05-10 13:59:13',	NULL,	'2024-05-10 16:39:12',	0),
+(2964,  1162,   1540,   -1, 'G95.2',    'Cord compression, unspecified', '2025-02-28 09:09:15',	26,	'2025-02-28 09:09:15',	0),
+(2965,  1162,   1757,    7, 'G95.89',   'Other specified diseases of spinal cord', '2025-02-28 09:09:15',	26,	'2025-02-28 09:09:15',	0),
+(1966,  1162,   1760,    7, 'G95.0',    'Syringomyelia and syringobulbia', '2025-02-28 09:09:15',	26,	'2025-02-28 09:09:15',	0);

db_management/opaldb/data/test/omi/omi_diagnosis.sql
@@ -8,8 +8,11 @@ INSERT INTO `Diagnosis` (`DiagnosisSerNum`, `PatientSerNum`, `SourceDatabaseSerN
 (3,	53,	1,	'22324',	'C16.9',	'Stomach, NOS',	'',	NULL,	NULL,	'2023-05-12 00:00:00',	'CronJob',	'2023-05-12 16:39:16',	'CronJob'),
 (7,	92,	1,	'22824',	'C50.8',	'Overlapping Ca of  breast',	'',	'IIB',	'T2, pN1a, M0',	'2014-03-12 00:00:00',	'CronJob',	'2018-03-27 12:54:54',	'CronJob'),
 (8,	92,	1,	'33208',	'C79.5',	'Secondary Ca of bone and bone marrow',	'',	NULL,	NULL,	'2016-09-28 00:00:00',	'CronJob',	'2016-01-21 18:59:22',	'CronJob'),
-(9,	59,	1,	'33209',	'C34.10',	'Upper lobe, lung',	'',	NULL,	NULL,	'2023-05-12 00:00:00',	'CronJob',	'2023-05-12 16:39:16',	'CronJob');
-
+(9,	59,	1,	'33209',	'C34.10',	'Upper lobe, lung',	'',	NULL,	NULL,	'2023-05-12 00:00:00',	'CronJob',	'2023-05-12 16:39:16',	'CronJob'),
+(11, 92, 7, '3', 'C50.112', 'Malignant neoplasm olf central portion of left female breast', '', '', '', '2025-02-27 10:42:32', 'interface-engine', '2025-02-27 10:42:37', 'interface-engine'),
+(12, 59, 7, '4', 'G20.B2', "Parkinson\'s disease with dyskinesia, with fluctuations", '', '', '', '2025-02-27 10:42:45', 'interface-engine', '2025-02-27 10:42:50', 'interface-engine'),
+(13, 93, 7, '5', 'G95.0', 'Syringomyelia and syringobulbia', '', '', '', '2025-02-27 11:12:21', 'interface-engine', '2025-02-27 11:12:26', 'interface-engine'),
+(14, 93, 7, '8', 'C07.', 'Malignant neoplasm of parotid gland', '', '', '', '2025-03-05 09:03:09', 'interface-engine',  '2025-03-05 10:15:08', 'interface-engine');
 -- Marge
 UPDATE `Diagnosis`
 SET `CreationDate` = DATE_ADD(now(), INTERVAL -7 DAY),

db_management/opaldb/data/test/01_sourcedatabase.sql
@@ -3,9 +3,10 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
 INSERT INTO `SourceDatabase` (`SourceDatabaseSerNum`, `SourceDatabaseName`, `Enabled`) VALUES
-(1,	'Aria',	1),
-(2,	'MediVisit', 1),
+(1,	'Aria',	0),
+(2,	'MediVisit', 0),
 (3,	'Mosaiq',	0),
-(4,	'Oacis',	1),
-(5,	'eRDV',	1),
-(6,	'ORMS',	1);
+(4,	'Oacis',	0),
+(5,	'eRDV',	0),
+(6,	'ORMS',	0),
+(7, 'OpenEMR', 1);

db_management/opaldb/data/test/aliasexpression.sql
@@ -5014,4 +5014,38 @@ INSERT INTO `AliasExpression` (`AliasExpressionSerNum`, `AliasSerNum`, `masterSo
 (10056, 292, 7485, 'TV-FU', 'Dr. G. Zogopoulos - Surg Onc - Teleconsultation suivi', '2000-01-01 00:00:00', 62, '2024-01-22 13:36:09', 'buBbKCyUp8'),
 (10057, 292, 8216, 'TV-FU', 'Dr. G. Zogopoulos - Teleconsultation suivi', '2000-01-01 00:00:00', 62, '2024-01-22 13:36:09', 'buBbKCyUp8'),
 (10058, 49, 25235, 'CONSULT NEW OUT', 'CONSULT NEW OUT', '2000-01-01 00:00:00', 62, '2024-02-29 13:18:22', 'G81gpwZPXQ'),
-(10059, 49, 25278, 'CONSULT NEW IN', 'CONSULT NEW IN', '2000-01-01 00:00:00', 62, '2024-02-29 13:19:23', 'G81gpwZPXQ');
+(10059, 49, 25278, 'CONSULT NEW IN', 'CONSULT NEW IN', '2000-01-01 00:00:00', 62, '2024-02-29 13:19:23', 'G81gpwZPXQ'),
+(10060, 619, 28035, 'Consult_New_In', 'Consult New In', '2000-01-01 00:00:00', 25, '2025-02-28 10:40:47', '3psMnxpdTU'),
+(10061, 621, 28036, 'FOLLOWUP', 'Follow Up', '2000-01-01 00:00:00', 25, '2025-02-28 10:43:55', '3psMnxpdTU'),
+(10062, 620, 28037, 'MRI_B_and_S', 'MRI Brain and Spine', '2000-01-01 00:00:00', 25, '2025-02-28 10:41:59', '3psMnxpdTU'),
+(10063, 617, 28009, 'Medical Record', 'Medical Record', '2019-01-01 00:00:00', 25, '2025-02-27 12:21:40', 'Eoxp64i29S'),
+(10064, 617, 28033, 'Medical Record | New Consult', 'Medical Record | New Consult', '2019-01-01 00:00:00', 25, '2025-02-27 12:21:40', 'Eoxp64i29S'),
+(10065, 617, 28034, 'Medical Record | Follow Up', 'Medical Record | Follow Up', '2019-01-01 00:00:00', 25, '2025-02-27 12:21:40', 'Eoxp64i29S'),
+(10066, 618, 28008, 'Lab Report', 'Lab Report', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10067, 618, 28010, 'Advance Directive', 'Advance Directive', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10068, 618, 28011, 'Advance Directive | Do Not Resuscitate Order', 'Advance Directive | Do Not Resuscitate Order', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10069, 618, 28012, 'Advance Directive | Durable Power of Attorney', 'Advance Directive | Durable Power of Attorney', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10070, 618, 28013, 'Advance Directive | Living Will', 'Advance Directive | Living Will', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10071, 618, 28014, 'CCR', 'CCR', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10072, 618, 28015, 'CCD', 'CCD', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10073, 618, 28016, 'CCDA', 'CCDA', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10074, 618, 28017, 'Eye Module', 'Eye Module', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10075, 618, 28018, 'Eye Module | Communication - Eye', 'Eye Module | Communication - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10076, 618, 28019, 'Eye Module | Encounters - Eye', 'Eye Module | Encounters - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10077, 618, 28020, 'Eye Module | Imaging - Eye | OCT - Eye', 'Eye Module | Imaging - Eye | OCT - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10078, 618, 28021, 'Eye Module | Imaging - Eye | FA/ICG - Eye', 'Eye Module | Imaging - Eye | FA/ICG - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10079, 618, 28022, 'Eye Module | Imaging - Eye | External Photos - Eye', 'Eye Module | Imaging - Eye | External Photos - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10080, 618, 28023, 'Eye Module | Imaging - Eye | AntSeg Photos - Eye', 'Eye Module | Imaging - Eye | AntSeg Photos - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10081, 618, 28024, 'Eye Module | Imaging - Eye | Optic Disc - Eye', 'Eye Module | Imaging - Eye | Optic Disc - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10082, 618, 28025, 'Eye Module | Imaging - Eye | Fundus - Eye', 'Eye Module | Imaging - Eye | Fundus - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10083, 618, 28026, 'Eye Module | Imaging - Eye | Radiology - Eye', 'Eye Module | Imaging - Eye | Radiology - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10084, 618, 28027, 'Eye Module | Imaging - Eye | VF - Eye', 'Eye Module | Imaging - Eye | VF - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10085, 618, 28028, 'Eye Module | Imaging - Eye | Drawings - Eye', 'Eye Module | Imaging - Eye | Drawings - Eye', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10086, 618, 28029, 'Onsite Portal', 'Onsite Portal', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10087, 618, 28030, 'Onsite Portal | Patient', 'Onsite Portal | Patient', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10088, 618, 28031, 'Onsite Portal | Reviewed', 'Onsite Portal | Reviewed', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10089, 618, 28032, 'Invoices', 'Invoices', '2019-01-01 00:00:00', 26, '2025-02-27 12:35:15', 'nGTPRYdsGE'),
+(10090, 614, 28038, 'ct_sim_tasked', 'CT Sim (TASKED)', '2000-01-01 00:00:00', 22, '2025-03-05 11:09:21', 'yuDs5tNDrJ'),
+(10091, 614, 28039, '.ebc_daily_rx', '.EBC-Daily Rx', '2000-01-01 00:00:00', 22, '2025-03-05 11:09:23', 'GrRGaTVtWm'),
+(10092, 614, 28040, 'intra_treat_out', 'INTRA TREAT OUT', '2000-01-01 00:00:00', 22, '2025-03-05 11:09:24', 'RWuRXxWcQB'),
+(10093, 614, 28041, 'follow_up_in_less_30_days', 'FOLLOW UP IN LESS/30DAYS', '2000-01-01 00:00:00', 22, '2025-03-05 11:09:25', 'FuqEGE2jgY');

db_management/opaldb/data/test/mastersourcediagnosis.sql
@@ -1751,4 +1751,10 @@ INSERT INTO `masterSourceDiagnosis` (`ID`, `externalId`, `code`, `description`, 
 (1753,	'ICD-O-2',	'C76.7',	'Other ill-defined sites',	1,	0,	'',	'2008-01-27 04:27:22',	'MIGRATION_MASTER_SOURCE_2021',	'2008-01-27 09:27:22',	'MIGRATION_MASTER_SOURCE_2021'),
 (1754,	'ICD-O-2',	'C80',	'UNKNOWN PRIMARY SITE',	1,	0,	'',	'2008-01-27 04:27:22',	'MIGRATION_MASTER_SOURCE_2021',	'2008-01-27 09:27:22',	'MIGRATION_MASTER_SOURCE_2021'),
 (1755,	'ICD-O-2',	'C80.9',	'Unknown primary site',	1,	0,	'',	'2008-01-27 04:27:22',	'MIGRATION_MASTER_SOURCE_2021',	'2008-01-27 09:27:22',	'MIGRATION_MASTER_SOURCE_2021'),
-(1756,	'ICD-10',	'R31.9',	'Hematuria - unspecified',	1,	0,	'',	'2024-05-10 04:27:22',	'MIGRATION_MASTER_SOURCE_2021',	'2024-05-10 09:27:22',	'MIGRATION_MASTER_SOURCE_2021');
+(1756,	'ICD-10',	'R31.9',	'Hematuria - unspecified',	1,	0,	'',	'2024-05-10 04:27:22',	'MIGRATION_MASTER_SOURCE_2021',	'2024-05-10 09:27:22',	'MIGRATION_MASTER_SOURCE_2021'),
+(1757, 'G95.89', 'G95.89', 'Other specified diseases of spinal cord', 7, 0, '', '2025-02-27 10:39:12', 'interface-engine', '2025-02-27 10:39:17', 'interface-engine'),
+(1758, 'C50.112', 'C50.112', 'Malignant neoplasm of central portion of left female breast', 7, 0, '', '2025-02-27 10:42:32', 'interface-engine', '2025-02-27 10:42:36', 'interface-engine'),
+(1759, 'G20.B2', 'G20.B2', "Parkinson\'s disease with dyskinesia, with fluctuations", 7, 0, '', '2025-02-27 10:42:45', 'interface-engine', '2025-02-27 10:42:50', 'interface-engine'),
+(1760, 'G95.0', 'G95.0', 'Syringomyelia and syringobulbia', 7, 0, '', '2025-02-27 11:12:21', 'interface-engine', '2025-02-27 11:12:26', 'interface-engine'),
+(1761, 'C07.', 'C07.', 'Malignant neoplasm of parotid gland', 7, 0, '', '2025-03-05 09:03:09', 'interface-engine', '2025-03-05 09:56:15', 'interface-engine'),
+(1762, 'C18.9', 'C18.9', 'Malignant neoplasm of colon, unspecified', 7, 0, '', '2025-03-05 23:03:12', 'interface-engine', '2025-03-05 23:40:16', 'interface-engine');

db_management/opaldb/data/test/omi/omi_01_patient_identifiers.sql
@@ -5,11 +5,7 @@
 -- OPAL MEDICAL INSTITUTION
 
 INSERT INTO `Hospital_Identifier_Type` (`Hospital_Identifier_Type_Id`, `Code`, `ADT_Web_Service_Code`, `Description_EN`, `Description_FR`) VALUES
-(1,	'RVH',	'MR_PCS',	'Opal General Hospital 1 (RVH)',	'Hôpital général Opal 1 (HRV)'),
-(2,	'MGH',	'MG_PCS',	'Opal General Hospital 2 (MGH)',	'Hôpital général Opal 2 (HGM)'),
-(3,	'MCH',	'MC_ADT',	"Opal Children\'s Hospital",	"L\'Hôpital Opal pour enfants"),
-(4,	'LAC',	'LC_ADT',	'Opal General Hospital 3 (LAC)',	'Hôpital général Opal 3 (LAC)'),
-(5,	'CRE',	'CR_ADT',	'Opal General Hospital 4 (CRE)',	'Hôpital général Opal 4 (CRE)');
+(1,	'ODH',	'MR_PCS',	'Opal Demo Hospital (ODH)',	"Hôpital démo d\'Opal (HDO)");
 
 -- Some dates are calculated relative to current to maintain constant age
 INSERT INTO `Patient` (`PatientSerNum`, `PatientAriaSer`, `PatientId`, `PatientId2`, `FirstName`, `LastName`, `Alias`, `ProfileImage`, `Sex`, `DateOfBirth`, `Age`, `TelNum`, `EnableSMS`, `Email`, `Language`, `SSN`, `AccessLevel`, `RegistrationDate`, `ConsentFormExpirationDate`, `BlockedStatus`, `StatusReasonTxt`, `DeathDate`, `SessionId`, `LastUpdated`, `TestUser`, `TermsAndAgreementSign`, `TermsAndAgreementSignDateTime`) VALUES
@@ -22,8 +18,8 @@ CONCAT(DATE_FORMAT(DATE_ADD(NOW(), INTERVAL -14 YEAR), '%Y'), '-02-23') - INTERV
 (55,	0,	'',	'',	'Mona',	    'Simpson',	'mona_test',	NULL,	'Female', '1940-03-15 00:00:00',	0,	5144758941,	0,	'mona@opalmedapps.ca',	'EN',	'SIMM40531599',	'1',	DATE_ADD(NOW(), INTERVAL -1 YEAR),	'2019-01-01 00:00:00',	1,	'Deceased',	DATE_ADD(CURRENT_DATE(), INTERVAL -2 YEAR),	'',	'2023-05-25 00:00:00',	1,	1,	DATE_ADD(NOW(), INTERVAL -1 YEAR)),
 (59,	0,	'',	'',	'Rory',	"O\'Brien",	'rory_test',	NULL,	'Other',
 CONCAT(DATE_FORMAT(DATE_ADD(NOW(), INTERVAL -52 YEAR), '%Y'), '-06-11') - INTERVAL (DATE_FORMAT(NOW(), '%m%d') < '0611') YEAR,	0,	5557654321,	0,	'rory@opalmedapps.ca',	'EN',	'OBRR72061199',	'3',	DATE_ADD(NOW(), INTERVAL -1 MONTH),	'2019-01-01 00:00:00',	1,	'',	'0000-00-00 00:00:00',	'',	'2024-09-09 00:00:00',	1,	1,	DATE_ADD(NOW(), INTERVAL -1 MONTH)),
-(92, 43235, '1092300', '5024737', 'Laurie', 'Hendren', 'Pointer Lady', '', 'Female', '1958-12-13 00:00:00', 64, 5144415642, 1, 'laurie@opalmedapps.ca', 'EN', 'HENL58621319', '3', '2018-01-01 00:00:00', '2019-01-01 00:00:00', 0, 'Unlocked by Johns Request', '2019-05-27 00:00:00', '', '2024-04-18 17:59:43', 0, NULL, NULL),
-(93,    0,  '', '', 'Bobby',    'Jones',     'bobby_jones_foundation', NULL, 'Male', '1985-01-01 00:00:00', 40, 0, 0, 'bobbyjones@opalmedapps.ca', 'EN', '', '3', '2025-01-01 00:00:00', '2025-01-01 00:00:00', 0, '', '0000-00-00 00:00:00', '', '2025-02-13 17:59:43', 1, 1,	DATE_ADD(NOW(), INTERVAL -1 YEAR));
+(92, 43235, '1092300', '5024737', 'Laurie', 'Opal', 'Pointer Lady', '', 'Female', '1958-12-13 00:00:00', 64, 5144415642, 1, 'laurie@opalmedapps.ca', 'EN', 'OPAL58621325', '3', '2018-01-01 00:00:00', '2019-01-01 00:00:00', 0, 'Unlocked by Johns Request', '2019-05-27 00:00:00', '', '2024-04-18 17:59:43', 0, NULL, NULL),
+(93,    0,  '', '', 'John',    'Smith',     'bobby_jones_foundation', NULL, 'Male', '1985-01-01 00:00:00', 40, 0, 0, 'john@opalmedapps.ca', 'EN', '', '3', '2025-01-01 00:00:00', '2025-01-01 00:00:00', 0, '', '0000-00-00 00:00:00', '', '2025-02-13 17:59:43', 1, 1,	DATE_ADD(NOW(), INTERVAL -1 YEAR));
 
 UPDATE `Patient` SET `Age` = DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), `DateOfBirth`)), '%Y') + 0 where PatientSerNum <> 92;
 
@@ -37,17 +33,13 @@ INSERT INTO `PatientControl` (`PatientSerNum`, `PatientUpdate`, `LastTransferred
 (93,	1,	'2025-02-13 00:56:01',	'2025-02-13 08:56:01',	0);
 
 INSERT INTO `Patient_Hospital_Identifier` (`Patient_Hospital_Identifier_Id`, `PatientSerNum`, `Hospital_Identifier_Type_Code`, `MRN`, `Is_Active`) VALUES
-(1,	51,	'RVH',	'9999996',	1),
-(2,	52,	'RVH',	'9999997',	1),
-(3,	52,	'MGH',	'9999998',	1),
-(4,	53,	'MCH',	'9999996',	1),
-(6,	55,	'RVH',	'9999993',	1),
-(7,	55,	'MCH',	'5407383',	1),
-(8,	51,	'LAC',	'0389731',	1),
-(12, 92, 'MGH',	'5024737',	1),
-(13, 92, 'RVH',	'1092300',	1),
-(14, 59, 'RVH',	'9999989',	1),
-(15, 93, 'RVH',	'9999994',	1);
+(1,	51,	'ODH',	'9999996',	1),
+(2,	52,	'ODH',	'9999997',	1),
+(4,	53,	'ODH',	'9999995',	1),
+(6,	55,	'ODH',	'9999993',	1),
+(13, 92, 'ODH',	'1092300',	1),
+(14, 59, 'ODH',	'9999989',	1),
+(15, 93, 'ODH',	'9999994',	1);
 
 INSERT INTO `Users` (`UserSerNum`, `UserType`, `UserTypeSerNum`, `Username`, `Password`, `SessionId`, `LastUpdated`) VALUES
 (1,	'Patient',	51,	'QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59'),

db_management/opaldb/data/test/omi/ori_01_patient_identifiers.sql
@@ -17,9 +17,9 @@ INSERT INTO `PatientControl` (`PatientSerNum`, `PatientUpdate`, `LastTransferred
 (58,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0);
 
 INSERT INTO `Patient_Hospital_Identifier` (`Patient_Hospital_Identifier_Id`, `PatientSerNum`, `Hospital_Identifier_Type_Code`, `MRN`, `Is_Active`) VALUES
-(9, 56, 'RVH',  '9999998',  1),
-(10,	57,	'MCH',	'9999999',	1),
-(11,	58,	'RVH',	'9999991',	1);
+(9, 56, 'ODH',  '9999998',  1),
+(10,	57,	'ODH',	'9999999',	1),
+(11,	58,	'ODH',	'9999991',	1);
 
 INSERT INTO `Users` (`UserSerNum`, `UserType`, `UserTypeSerNum`, `Username`, `Password`, `SessionId`, `LastUpdated`) VALUES
 (5,	'Patient',	56,	'ZYHAjhNy6hhr4tOW8nFaVEeKngt1',	'c9a29c53a3c5b4339ba51352e16ebbe797aeaa0d574c1724aa1779535ae2ede216328dca4d754c40841b49719a6ff5e1554fa7a14da7567f1a9d7b905bf95aab',	'',	'2021-08-10 16:24:59');

commit 9849390923181f4c138f3d739c8c8b8d67e56e9a

fix: fix broken SQL scripts (#479)
Fix broken SQL scripts caused by de4190f and 7a39caa.

db_management/opaldb/data/initial/diagnosis.sql
@@ -50,7 +50,7 @@ INSERT INTO `DiagnosisTranslation` (`DiagnosisTranslationSerNum`, `AliasName`, `
 (1159,	'',	NULL,	'Sarcoma - Lower limb',	'Sarcome - membre inférieur',	'<p><span style=\"font-size: 14px;float: none;\">This is a diagnosis of upper limb sarcoma.</span><!--EndFragment--><br/><br/></p>',	'<p>Ceci est un diagnostique de sarcome des membres inférieurs.</p>',	'',	'2020-02-18 19:16:11',	NULL,	'2020-02-19 05:17:36',	'7CigzsWZsM'),
 (1160,	'',	NULL,	'Kaposi\'s sarcoma',	'Maladie de Kaposi',	'<p>This is a diagnosis of Kaposi\'s sarcoma.</p>',	'<p>Ceci est un diagnostic de la maladie de Kaposi.</p>',	'',	'2020-02-18 19:19:41',	NULL,	'2020-02-19 05:19:41',	'7CigzsWZsM'),
 (1161,	'',	NULL,	'Hematuria',	'Hématurie',	'<p>A disorder characterized by laboratory test results that indicate blood in the urine.</p>',	"<p>Trouble caractérisé par des résultats de tests de laboratoire indiquant la présence de sang dans l\'urine.</p>",	'',	'2024-05-10 19:16:11',	NULL,	'2024-05-10 05:17:36',	NULL),
-(1162,  '', NULL,   'Syringomyelia', 'Syringomyelie', '<p>Syringomyelia<br/></p>', '<p>Syringomyelie<br/></p>', '', '2025-02-27 11:14:06', 25, '2025-02-27 11:14:06', '28U2zEskKtB');
+(1162,  '', NULL,   'Syringomyelia', 'Syringomyelie', '<p>Syringomyelia<br/></p>', '<p>Syringomyelie<br/></p>', '', '2025-02-27 11:14:06', NULL, '2025-02-27 11:14:06', '28U2zEskKtB');
 
 INSERT INTO `DiagnosisCode` (`DiagnosisCodeSerNum`, `DiagnosisTranslationSerNum`, `SourceUID`, `Source`, `DiagnosisCode`, `Description`, `DateAdded`, `LastUpdatedBy`, `LastUpdated`, `SessionId`) VALUES
 (228,	1131,	-9173,	-1,	'C33',	'Ca of trachea',	'2018-01-12 14:29:58',	NULL,	'2023-01-12 16:39:12',	NULL),
@@ -1488,6 +1488,6 @@ INSERT INTO `DiagnosisCode` (`DiagnosisCodeSerNum`, `DiagnosisTranslationSerNum`
 (2960,	1129,	-11430,	-1,	'inflammatory bre',	'Ca of upper-outer quadrant of breast',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2961,	1129,	-9106,	-1,	'T1c N0 (0/6)M0',	'Ca of breast, unspecified',	'2021-08-27 13:59:13',	NULL,	'2023-01-12 16:39:12',	NULL),
 (2962,	1161,	-11431,	-1,	'R31.9',	'Hematuria, unspecified',	'2024-05-10 13:59:13',	NULL,	'2024-05-10 16:39:12',	0),
-(2964,  1162,   1540,   -1, 'G95.2',    'Cord compression, unspecified', '2025-02-28 09:09:15',	26,	'2025-02-28 09:09:15',	0),
-(2965,  1162,   1757,    7, 'G95.89',   'Other specified diseases of spinal cord', '2025-02-28 09:09:15',	26,	'2025-02-28 09:09:15',	0),
-(1966,  1162,   1760,    7, 'G95.0',    'Syringomyelia and syringobulbia', '2025-02-28 09:09:15',	26,	'2025-02-28 09:09:15',	0);
+(2964,  1162,   1540,   -1, 'G95.2',    'Cord compression, unspecified', '2025-02-28 09:09:15',	NULL,	'2025-02-28 09:09:15',	0),
+(2965,  1162,   1757,    -1, 'G95.89',   'Other specified diseases of spinal cord', '2025-02-28 09:09:15',	NULL,	'2025-02-28 09:09:15',	0),
+(2966,  1162,   1760,    -1, 'G95.0',    'Syringomyelia and syringobulbia', '2025-02-28 09:09:15',	NULL,	'2025-02-28 09:09:15',	0);

db_management/opaldb/data/test/omi/omi_diagnosis.sql
@@ -13,6 +13,7 @@ INSERT INTO `Diagnosis` (`DiagnosisSerNum`, `PatientSerNum`, `SourceDatabaseSerN
 (12, 59, 7, '4', 'G20.B2', "Parkinson\'s disease with dyskinesia, with fluctuations", '', '', '', '2025-02-27 10:42:45', 'interface-engine', '2025-02-27 10:42:50', 'interface-engine'),
 (13, 93, 7, '5', 'G95.0', 'Syringomyelia and syringobulbia', '', '', '', '2025-02-27 11:12:21', 'interface-engine', '2025-02-27 11:12:26', 'interface-engine'),
 (14, 93, 7, '8', 'C07.', 'Malignant neoplasm of parotid gland', '', '', '', '2025-03-05 09:03:09', 'interface-engine',  '2025-03-05 10:15:08', 'interface-engine');
+
 -- Marge
 UPDATE `Diagnosis`
 SET `CreationDate` = DATE_ADD(now(), INTERVAL -7 DAY),