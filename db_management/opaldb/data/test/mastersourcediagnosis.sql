303141e7e5160eb572566baf44541411ba6b4820
Zeyu Dou <dou3180@hotmail.com>
2025-05-28 21:26:33 -0400

    appointment & study

---
+++ db_management/opaldb/data/test/mastersourcediagnosis.sql
@@ -0,0 +1,11 @@
+-- SPDX-FileCopyrightText: Copyright (C) 2023 Opal Health Informatics Group at the Research Institute of the McGill University Health Centre <john.kildea@mcgill.ca>
+--
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+INSERT INTO `masterSourceDiagnosis` (`ID`, `externalId`, `code`, `description`, `source`, `deleted`, `deletedBy`, `creationDate`, `createdBy`, `lastUpdated`, `updatedBy`) VALUES
+(1757, 'G95.89', 'G95.89', 'Other specified diseases of spinal cord', 7, 0, '', '2025-02-27 10:39:12', 'interface-engine', '2025-02-27 10:39:17', 'interface-engine'),
+(1758, 'C50.112', 'C50.112', 'Malignant neoplasm of central portion of left female breast', 7, 0, '', '2025-02-27 10:42:32', 'interface-engine', '2025-02-27 10:42:36', 'interface-engine'),
+(1759, 'G20.B2', 'G20.B2', "Parkinson\'s disease with dyskinesia, with fluctuations", 7, 0, '', '2025-02-27 10:42:45', 'interface-engine', '2025-02-27 10:42:50', 'interface-engine'),
+(1760, 'G95.0', 'G95.0', 'Syringomyelia and syringobulbia', 7, 0, '', '2025-02-27 11:12:21', 'interface-engine', '2025-02-27 11:12:26', 'interface-engine'),
+(1761, 'C07.', 'C07.', 'Malignant neoplasm of parotid gland', 7, 0, '', '2025-03-05 09:03:09', 'interface-engine', '2025-03-05 09:56:15', 'interface-engine'),
+(1762, 'C18.9', 'C18.9', 'Malignant neoplasm of colon, unspecified', 7, 0, '', '2025-03-05 23:03:12', 'interface-engine', '2025-03-05 23:40:16', 'interface-engine');
