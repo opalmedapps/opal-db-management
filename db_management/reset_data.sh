854ca20b0a219eb14eb938bdd2f184bf46bea414
Matthias Schoettle <git@mattsch.com>
2025-06-18 16:08:50 -0400

    fix(dat): add only those test patient that are in test data

--- db_management/reset_data.sh
+++ db_management/reset_data.sh
@@ -40,14 +40,6 @@ declare -a commands=(
     "python -m db_management.run_sql_scripts OpalDB db_management/opaldb/data/truncate/"
     "python -m db_management.run_sql_scripts QuestionnaireDB db_management/questionnairedb/data/truncate/"
     "python -m db_management.run_sql_scripts OrmsDatabase db_management/ormsdb/data/truncate/"
-)
-
-# Insert fake patients if in CI to avoid foreign key constraint errors
-if [[ -n "${CI:-}" ]]; then
-    commands+=("python -m db_management.run_sql_scripts OpalDB db_management/opaldb/data/test/patients/")
-fi
-
-commands+=(
     # QuestionnaireDB needs to come first due to references from OpalDB to QuestionnaireDB
     "python -m db_management.run_sql_scripts QuestionnaireDB db_management/questionnairedb/data/initial/"
     "python -m db_management.run_sql_scripts QuestionnaireDB db_management/questionnairedb/data/test/$institution/"
