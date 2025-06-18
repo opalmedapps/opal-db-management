b2f8230fad0dedf1bd09100f7096df14b6dbcb82
Matthias Schoettle <git@mattsch.com>
2025-06-18 14:54:36 -0400

    add corresponding test group expressions

---
+++ db_management/opaldb/data/test/testgroupexpression.sql
@@ -0,0 +1,9 @@
+-- SPDX-FileCopyrightText: Copyright (C) 2023 Opal Health Informatics Group at the Research Institute of the McGill University Health Centre <john.kildea@mcgill.ca>
+--
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+INSERT INTO `TestGroupExpression` (`TestGroupExpressionSerNum`, `TestCode`, `ExpressionName`, `DateAdded`, `LastPublished`, `LastUpdatedBy`, `SourceDatabaseSerNum`, `LastUpdated`, `SessionId`) VALUES
+(9, 'COV9', 'CORONAVIRUS (SARS-COV-2) (TAAN)', '2022-01-21 21:05:26', NULL, NULL, 4, '2022-01-21 21:05:26', NULL),
+(327, 'NA', 'Na', '2022-01-21 23:31:43', NULL, NULL, 4, '2022-01-21 23:31:43', NULL),
+(328, 'K', 'K', '2022-01-21 23:31:43', NULL, NULL, 4, '2022-01-21 23:31:43', NULL),
+(339, 'MG', 'MG', '2022-01-21 23:33:46', NULL, NULL, 4, '2022-01-21 23:33:46', NULL);
