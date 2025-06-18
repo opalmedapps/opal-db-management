71ea124b7d3d1d990955cd9005e847865fcc34f3
Matthias Schoettle <git@mattsch.com>
2025-06-18 15:18:20 -0400

    fix source DB

--- db_management/opaldb/data/test/testexpression.sql
+++ db_management/opaldb/data/test/testexpression.sql
@@ -3,13 +3,13 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
 INSERT INTO `TestExpression` (`TestExpressionSerNum`, `TestControlSerNum`, `TestCode`, `ExpressionName`, `DateAdded`, `LastPublished`, `LastUpdatedBy`, `SourceDatabaseSerNum`, `LastUpdated`, `SessionId`, `externalId`, `deleted`, `deletedBy`, `createdBy`, `updatedBy`) VALUES
-(8,	32,	'NA',	'SODIUM',	'0000-00-00 00:00:00',	'2000-01-01 00:00:00',	NULL,	4,	'2023-01-12 16:39:14',	'KRKB2sqqIf',	'-1',	0,	'',	'cronjob',	'cronjob'),
-(62,	6,	'WBC',	'White Blood Cell',	'0000-00-00 00:00:00',	'2000-01-01 00:00:00',	NULL,	4,	'2023-01-12 16:39:14',	'koziqvQOdO',	'-1',	0,	'',	'cronjob',	'cronjob'),
-(65,	29,	'HCT',	'Hematocrit',	'0000-00-00 00:00:00',	'2000-01-01 00:00:00',	NULL,	4,	'2023-01-12 16:39:14',	'W2RB6YcVts',	'-1',	0,	'',	'cronjob',	'cronjob'),
-(70,	30,	'PLT',	'Platelet',	'0000-00-00 00:00:00',	'2000-01-01 00:00:00',	NULL,	4,	'2023-01-12 16:39:14',	'SpKKBu6Pwm',	'-1',	0,	'',	'cronjob',	'cronjob'),
-(78,	49,	'NE#',	'Abs. Neutrophil Automated',	'0000-00-00 00:00:00',	'2000-01-01 00:00:00',	NULL,	4,	'2023-01-12 16:39:14',	'6VQxaCZO3c',	'-1',	0,	'',	'cronjob',	'cronjob'),
-(85,	37,	'MG',	'MAGNESIUM',	'0000-00-00 00:00:00',	'2000-01-01 00:00:00',	NULL,	4,	'2023-01-12 16:39:14',	'fMXeUDEQrI',	'-1',	0,	'',	'cronjob',	'cronjob'),
-(86,	33,	'K',	'POTASSIUM',	'0000-00-00 00:00:00',	'2000-01-01 00:00:00',	NULL,	4,	'2023-01-12 16:39:14',	'fMXeUDEQrI',	'-1',	0,	'',	'cronjob',	'cronjob'),
-(145,	31,	'MEOS',	'Eosinophil Manual',	'0000-00-00 00:00:00',	'2000-01-01 00:00:00',	NULL,	4,	'2023-01-12 16:39:14',	'V4DNwi6oHj',	'-1',	0,	'',	'cronjob',	'cronjob'),
-(233,	52,	'COV9',	'Coronavirus (SARS-CoV-2) (TAAN)',	'0000-00-00 00:00:00',	'2000-01-01 00:00:00',	NULL,	4,	'2023-01-12 16:39:14',	'K4ZGVx2GcI',	'-1',	0,	'',	'cronjob',	'cronjob'),
-(301,	36,	'NCA',	'Ca Ionized Normalized',	'0000-00-00 00:00:00',	'2000-01-01 00:00:00',	NULL,	4,	'2023-01-12 16:39:14',	'5Xe8UyUn9O',	'-1',	0,	'',	'cronjob',	'cronjob');
+(8, 32, 'NA', 'SODIUM', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'KRKB2sqqIf', '-1', 0, '', 'cronjob', 'cronjob'),
+(62, 6, 'WBC', 'White Blood Cell', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'koziqvQOdO', '-1', 0, '', 'cronjob', 'cronjob'),
+(65, 29, 'HCT', 'Hematocrit', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'W2RB6YcVts', '-1', 0, '', 'cronjob', 'cronjob'),
+(70, 30, 'PLT', 'Platelet', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'SpKKBu6Pwm', '-1', 0, '', 'cronjob', 'cronjob'),
+(78, 49, 'NE#', 'Abs. Neutrophil Automated', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', '6VQxaCZO3c', '-1', 0, '', 'cronjob', 'cronjob'),
+(85, 37, 'MG', 'MAGNESIUM', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'fMXeUDEQrI', '-1', 0, '', 'cronjob', 'cronjob'),
+(86, 33, 'K', 'POTASSIUM', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'fMXeUDEQrI', '-1', 0, '', 'cronjob', 'cronjob'),
+(145, 31, 'MEOS', 'Eosinophil Manual', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'V4DNwi6oHj', '-1', 0, '', 'cronjob', 'cronjob'),
+(233, 52, 'COV9', 'Coronavirus (SARS-CoV-2) (TAAN)', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'K4ZGVx2GcI', '-1', 0, '', 'cronjob', 'cronjob'),
+(301, 36, 'NCA', 'Ca Ionized Normalized', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', '5Xe8UyUn9O', '-1', 0, '', 'cronjob', 'cronjob');
--- db_management/opaldb/data/test/testgroupexpression.sql
+++ db_management/opaldb/data/test/testgroupexpression.sql
@@ -3,7 +3,7 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
 INSERT INTO `TestGroupExpression` (`TestGroupExpressionSerNum`, `TestCode`, `ExpressionName`, `DateAdded`, `LastPublished`, `LastUpdatedBy`, `SourceDatabaseSerNum`, `LastUpdated`, `SessionId`) VALUES
-(9, 'COV9', 'CORONAVIRUS (SARS-COV-2) (TAAN)', '2022-01-21 21:05:26', NULL, NULL, 4, '2022-01-21 21:05:26', NULL),
-(327, 'NA', 'Na', '2022-01-21 23:31:43', NULL, NULL, 4, '2022-01-21 23:31:43', NULL),
-(328, 'K', 'K', '2022-01-21 23:31:43', NULL, NULL, 4, '2022-01-21 23:31:43', NULL),
-(339, 'MG', 'MG', '2022-01-21 23:33:46', NULL, NULL, 4, '2022-01-21 23:33:46', NULL);
+(9, 'COV9', 'CORONAVIRUS (SARS-COV-2) (TAAN)', '2022-01-21 21:05:26', NULL, NULL, 7, '2022-01-21 21:05:26', NULL),
+(327, 'NA', 'Na', '2022-01-21 23:31:43', NULL, NULL, 7, '2022-01-21 23:31:43', NULL),
+(328, 'K', 'K', '2022-01-21 23:31:43', NULL, NULL, 7, '2022-01-21 23:31:43', NULL),
+(339, 'MG', 'MG', '2022-01-21 23:33:46', NULL, NULL, 7, '2022-01-21 23:33:46', NULL);
