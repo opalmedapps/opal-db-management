5e432c2555a8a3b417843ac4dacfce53e4843bb4
Matthias Schoettle <git@mattsch.com>
2025-06-19 14:44:24 -0400

    remove hardcoded patients from questionnaire patients since they get synced

--- db_management/questionnairedb/data/test/ohigph/patients.sql
+++ db_management/questionnairedb/data/test/ohigph/patients.sql
@@ -2,13 +2,4 @@
 --
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
--- Marge, Bart, Lisa
-INSERT INTO `patient` (`ID`, `hospitalId`, `externalId`, `deleted`, `deletedBy`, `creationDate`, `createdBy`, `lastUpdated`, `updatedBy`) VALUES
-(1,	-1,	51,	0,	'',	'2024-05-10 12:35:11',	'QUESTIONNAIRE_V2_AUTO_SYNC',	'2024-05-10 08:35:11',	'QUESTIONNAIRE_V2_AUTO_SYNC'),
-(3,	-1,	53,	0,	'',	'2024-05-10 12:35:11',	'QUESTIONNAIRE_V2_AUTO_SYNC',	'2024-05-10 08:35:11',	'QUESTIONNAIRE_V2_AUTO_SYNC'),
-(4,	-1,	54,	0,	'',	'2024-05-10 12:35:11',	'QUESTIONNAIRE_V2_AUTO_SYNC',	'2024-05-10 08:35:11',	'QUESTIONNAIRE_V2_AUTO_SYNC');
-
-UPDATE `patient`
-SET
-`creationDate` = now(),
-`lastUpdated` = now();
+-- patients get synced automatically via the DB event that runs periodically
