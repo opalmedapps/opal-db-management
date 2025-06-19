a5761adad8fca4db4ae64e395035e48cde7712e2
Matthias Schoettle <git@mattsch.com>
2025-06-19 12:29:30 -0400

    add test expression

--- db_management/opaldb/data/test/testexpression.sql
+++ db_management/opaldb/data/test/testexpression.sql
@@ -5,9 +5,11 @@
 INSERT INTO `TestExpression` (`TestExpressionSerNum`, `TestControlSerNum`, `TestCode`, `ExpressionName`, `DateAdded`, `LastPublished`, `LastUpdatedBy`, `SourceDatabaseSerNum`, `LastUpdated`, `SessionId`, `externalId`, `deleted`, `deletedBy`, `createdBy`, `updatedBy`) VALUES
 (8, 32, 'NA', 'SODIUM', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'KRKB2sqqIf', '-1', 0, '', 'cronjob', 'cronjob'),
 (62, 6, 'WBC', 'White Blood Cell', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'koziqvQOdO', '-1', 0, '', 'cronjob', 'cronjob'),
+(64, 28, 'HGB', 'Hemoglobin', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', '9DWhzu6u6w', '-1', 0, '', 'cronjob', 'cronjob'),
 (65, 29, 'HCT', 'Hematocrit', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'W2RB6YcVts', '-1', 0, '', 'cronjob', 'cronjob'),
 (70, 30, 'PLT', 'Platelet', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'SpKKBu6Pwm', '-1', 0, '', 'cronjob', 'cronjob'),
 (78, 49, 'NE#', 'Abs. Neutrophil Automated', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', '6VQxaCZO3c', '-1', 0, '', 'cronjob', 'cronjob'),
+(83, 27, 'ANRB', 'Abs. Nucleated RBC Auto', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'E0bftpt3nZ', '-1', 0, '', 'cronjob', 'cronjob'),
 (85, 37, 'MG', 'MAGNESIUM', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'fMXeUDEQrI', '-1', 0, '', 'cronjob', 'cronjob'),
 (86, 33, 'K', 'POTASSIUM', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'fMXeUDEQrI', '-1', 0, '', 'cronjob', 'cronjob'),
 (145, 31, 'MEOS', 'Eosinophil Manual', '0000-00-00 00:00:00', '2000-01-01 00:00:00', NULL, 7, '2023-01-12 16:39:14', 'V4DNwi6oHj', '-1', 0, '', 'cronjob', 'cronjob'),
