aeb261feea47c90a1441e7a1673c3f685753abf6
Matthias Schoettle <git@mattsch.com>
2025-07-03 15:13:06 -0400

    add back ohigph patient

--- db_management/opaldb/data/test/ohigph/ohigph_01_patient_identifiers.sql
+++ db_management/opaldb/data/test/ohigph/ohigph_01_patient_identifiers.sql
@@ -6,3 +6,15 @@
 
 INSERT INTO `Hospital_Identifier_Type` (`Hospital_Identifier_Type_Id`, `Code`, `ADT_Web_Service_Code`, `Description_EN`, `Description_FR`) VALUES
 (1,	'CHUSJ',	'CHUSJ',	'OHIG Pediatric Hospital',	'Hôpital Pédiatrique OHIG');
+
+
+INSERT INTO `Patient` (`PatientSerNum`, `PatientAriaSer`, `PatientId`, `PatientId2`, `FirstName`, `LastName`, `Alias`, `ProfileImage`, `Sex`, `DateOfBirth`, `Age`, `TelNum`, `EnableSMS`, `Email`, `Language`, `SSN`, `AccessLevel`, `RegistrationDate`, `ConsentFormExpirationDate`, `BlockedStatus`, `StatusReasonTxt`, `DeathDate`, `SessionId`, `LastUpdated`, `TestUser`, `TermsAndAgreementSign`, `TermsAndAgreementSignDateTime`) VALUES
+(54,	0,	'',	'',	'Lisa',	    'Simpson',	'lisa_test',	NULL,	'Female',	CONCAT(DATE_FORMAT(DATE_ADD(NOW(), INTERVAL -8 YEAR), '%Y'), '-05-09') - INTERVAL (DATE_FORMAT(NOW(), '%m%d') < '0509') YEAR,	0,	61292507111,	0,	'lisa@opalmedapps.ca',	'EN',	'SIML14550999',	'3',	DATE_ADD(NOW(), INTERVAL -1 DAY),	'2019-01-01 00:00:00',	0,	'',	'0000-00-00 00:00:00',	'',	'2023-05-25 00:00:00',	1,	NULL,	NULL);
+
+UPDATE `Patient` SET `Age` = DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), `DateOfBirth`)), '%Y') + 0;
+
+INSERT INTO `PatientControl` (`PatientSerNum`, `PatientUpdate`, `LastTransferred`, `LastUpdated`, `TransferFlag`) VALUES
+(54,	1,	'2021-09-30 00:56:01',	'2021-09-30 08:56:01',	0);
+
+INSERT INTO `Patient_Hospital_Identifier` (`Patient_Hospital_Identifier_Id`, `PatientSerNum`, `Hospital_Identifier_Type_Code`, `MRN`, `Is_Active`) VALUES
+(5,	54,	'CHUSJ',	'9999993',	1);
