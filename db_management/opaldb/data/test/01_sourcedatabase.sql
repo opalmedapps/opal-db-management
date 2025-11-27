0104833dfc4ef20fa570dbe2870eccc9c013f4cd
Matthias Schoettle <git@mattsch.com>
2025-11-27 14:50:41 -0500

    fix(data): change source DB ID to match the test expression default (#711)
    * fix(data): change source DB ID to match the test expression default
    
    * fix test data

--- db_management/opaldb/data/test/01_sourcedatabase.sql
+++ db_management/opaldb/data/test/01_sourcedatabase.sql
@@ -3,4 +3,4 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
 INSERT INTO `SourceDatabase` (`SourceDatabaseSerNum`, `SourceDatabaseName`, `Enabled`) VALUES
-(7, 'OpenEMR', 1);
+(4, 'OpenEMR', 1);
