6ba7f92846d2eea0f2d755f7e0a52d8a8a829b33
Matthias Schoettle <git@mattsch.com>
2025-12-08 16:44:44 -0500

    more updates

--- db_management/ormsdb/data/initial/initial.sql
+++ db_management/ormsdb/data/initial/initial.sql
@@ -3,30 +3,19 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
 INSERT INTO `Hospital` (`HospitalId`, `HospitalCode`, `HospitalName`, `Format`) VALUES
-(1,	'ODH',	'Opal Demo Hospital 1 (ODH)',	'^[0-9]{7}$'),
-(2,	'MGH',	'Opal General Hospital 2 (MGH)',	'^[0-9]{7}$'),
-(3,	'MCH',	'Opal Children\'s Hospital',	'^[0-9]{7}$'),
-(4,	'LAC', 'Opal General Hospital 3 (LAC)', '^[0-9]{7}$'),
-(5,	'CRE', 'Opal General Hospital 4 (CRE)', '^[0-9]{7}$');
+(1,	'ODH',	'Opal Demo Hospital 1 (OD)',	'^[0-9]{7}$'),
+(2,	'MGH',	'Opal General Hospital 2 (MGH)',	'^[0-9]{7}$')
+;
 
 REPLACE INTO `SpecialityGroup` (`SpecialityGroupId`, `HospitalId`, `SpecialityGroupCode`, `SpecialityGroupName`, `LastUpdated`) VALUES
-(1, 1, 'CCC', 'Cedars Cancer Centre', '2022-01-22 02:34:21'),
-(2, 1, 'MedClin', 'Medicine Clinics - RVH', '2022-01-22 02:34:21'),
-(3, 1, 'SurgClin', 'Surgical Clinics - RVH', '2022-01-22 02:34:21'),
-(4, 3, 'GI', 'Medicine Clinics - MGH', '2022-01-22 02:34:21'),
-(5, 2, 'NEPH', 'Nephrology', '2022-01-22 02:34:21');
+-- (1, 1, 'General', 'Opal Demo Hospital', '2022-01-22 02:34:21')
+;
 
 INSERT INTO `ClinicHub` (`ClinicHubId`, `SpecialityGroupId`, `ClinicHubName`, `LastUpdated`) VALUES
 (1, 1, 'D RC', '2022-01-22 02:34:26'),
 (2, 1, 'D S1', '2022-01-22 02:34:26'),
-(3, 1, 'D 02', '2022-01-22 02:34:26'),
-(4, 2, 'Cardiovascular Clinics', '2022-01-22 02:34:26'),
-(5, 2, 'CVIS - ID Clinics', '2022-01-22 02:34:26'),
-(6, 2, 'Medical Clinics', '2022-01-22 02:34:26'),
-(7, 3, 'Surgical Clinics - North', '2022-01-22 02:34:26'),
-(8, 3, 'Surgical Clinics - South', '2022-01-22 02:34:26'),
-(9, 4, 'IBD clinic', '2022-01-22 02:34:26'),
-(10, 5, 'Nephrology', '2022-01-22 02:34:26');
+(3, 1, 'D 02', '2022-01-22 02:34:26')
+;
 
 INSERT INTO `Insurance` (`InsuranceId`, `InsuranceCode`, `InsuranceName`, `Format`) VALUES
 (1,	'RAMA',	'Régie de l\'assurance maladie de l\'Alberta',	'^[0-9]{9}$'),
